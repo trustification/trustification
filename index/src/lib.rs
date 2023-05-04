430d063ada86860ec2db922a81d116fa50fecf70
Ulf Lilleengen <lulf@redhat.com>
2023-05-04 12:08:32 +0200

    fix workspace member names

--- index/src/lib.rs
+++ index/src/lib.rs
@@ -1,22 +1,58 @@
+use std::path::PathBuf;
+
+
 pub struct Index {
 }
 
 pub enum Error {
+    Open,
     Other
 }
 
 impl Index {
-    pub fn new() -> Self {
+    pub fn new<T: AsRef<Path>>(path: T) -> Result<Self, Error> {
+        let connection = sqlite::open(path).map_err(|_| Error::Open)?;
         Self {
-
+            connection,
         }
     }
 
-    pub async fn query_purl(&self, purl: &str) -> Result<Vec<u8>, Error> {
+    pub fn new_with_handle(connection: sqlite::Connection) -> Self {
+        Self {
+            connection
+        }
+    }
+
+    pub async fn query_purl(&mut self, purl: &str) -> Result<Vec<u8>, Error> {
         todo!()
     }
 
-    pub async fn query_hash(&self, hash: &str) -> Result<Vec<u8>, Error> {
+    pub async fn query_hash(&mut self, hash: &str) -> Result<Vec<u8>, Error> {
         todo!()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn init(conn: &mut sqlite::Connection) {
+        let schema = File::open("../schema.sql");
+        conn.execute(schema).unwrap();
+
+        let test_data = "INSERT into sboms VALUES (\"purl1\", \"116940abae80491f5357f652e55c48347dd7a2a1ff27df578c4572a383373c70\", \"116940abae80491f5357f652e55c48347dd7a2a1ff27df578c4572a383373c70\")";
+        conn.execute(test_data).unwrap();
+    }
+
+    #[tokio::test]
+    fn test_query() {
+        let conn = sqlite::open(":memory:").unwrap();
+        init(&mut conn);
+
+        let mut index = Index::new_with_handle(conn);
+
+        let result = index.query_purl("purl1");
+        assert!(result.is_ok());
     }
 }
