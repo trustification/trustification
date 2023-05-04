7b978761b8808fd88d81efa15b137983c577966f
Ulf Lilleengen <lulf@redhat.com>
2023-05-04 14:06:07 +0200

    add initial storage module

--- api/src/server.rs
+++ api/src/server.rs
@@ -6,6 +6,7 @@ use axum::{
     Json, Router,
 };
 use bombastic_index::Index;
+use bombastic_storage::{Config as StorageConfig, Storage};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::path::Path;
@@ -13,18 +14,25 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 // TODO: Figure out a way to not lock since we use it read only
-type SharedState = Arc<Mutex<Index>>;
+type SharedState = Arc<Mutex<AppState>>;
+
+pub struct AppState {
+    index: Index,
+    storage: Storage,
+}
 
 pub async fn run<T: AsRef<Path>, B: Into<SocketAddr>>(
     index: T,
     bind: B,
 ) -> Result<(), anyhow::Error> {
-    let index = Arc::new(Mutex::new(Index::new(index)?));
+    let storage = Storage::new(StorageConfig::new_minio_test())?;
+    let index = Index::new(index)?;
+    let state = Arc::new(Mutex::new(AppState { storage, index }));
 
     let app = Router::new()
         .route("/api/v1/sbom", get(fetch_sbom))
         .route("/api/v1/sbom", post(publish_sbom))
-        .with_state(index);
+        .with_state(state);
 
     let addr = bind.into();
     tracing::debug!("listening on {}", addr);
@@ -35,7 +43,7 @@ pub async fn run<T: AsRef<Path>, B: Into<SocketAddr>>(
 }
 
 async fn fetch_sbom(
-    State(index): State<SharedState>,
+    State(state): State<SharedState>,
     Query(params): Query<FetchParams>,
 ) -> (StatusCode, Json<FetchResponse>) {
     // TODO: Implement
@@ -52,7 +60,7 @@ struct FetchParams {
 struct FetchResponse {}
 
 async fn publish_sbom(
-    State(index): State<SharedState>,
+    State(state): State<SharedState>,
     Json(payload): Json<PublishRequest>,
 ) -> (StatusCode, Json<PublishResponse>) {
     let response = PublishResponse {};
--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,15 +1,70 @@
+use s3::creds::error::CredentialsError;
+use s3::error::S3Error;
+use s3::Bucket;
+use std::fmt::Display;
+
+pub use s3::creds::Credentials;
+pub use s3::Region;
+
 pub struct Storage {
+    bucket: Bucket,
 }
 
+pub struct Config {
+    bucket_name: String,
+    region: Region,
+    credentials: Credentials,
+}
+
+impl Config {
+    pub fn new_minio_test() -> Self {
+        Config {
+            bucket_name: "test-rust-s3".to_string(),
+            region: Region::Custom {
+                region: "eu-central-1".to_owned(),
+                endpoint: "http://localhost:9000".to_owned(),
+            },
+            credentials: Credentials::default().unwrap(),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum Error {
-    Other
+    Internal,
+    Credentials(CredentialsError),
+    S3(S3Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Internal => write!(f, "Internal error"),
+            Self::Credentials(e) => write!(f, "{}", e),
+            Self::S3(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<CredentialsError> for Error {
+    fn from(e: CredentialsError) -> Self {
+        Self::Credentials(e)
+    }
+}
+
+impl From<S3Error> for Error {
+    fn from(e: S3Error) -> Self {
+        Self::S3(e)
+    }
 }
 
 impl Storage {
-    pub fn new() -> Self {
-        Self {
-
-        }
+    pub fn new(config: Config) -> Result<Self, Error> {
+        let bucket =
+            Bucket::new(&config.bucket_name, config.region, config.credentials)?.with_path_style();
+        Ok(Self { bucket })
     }
 
     pub async fn put(&self, key: &str, value: &[u8]) -> Result<(), Error> {
