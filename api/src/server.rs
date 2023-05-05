8f58b0507f937c3f088f0eea27a3352e824ce013
Ulf Lilleengen <lulf@redhat.com>
2023-05-05 09:31:31 +0200

    add index and storage put/get

--- api/src/server.rs
+++ api/src/server.rs
@@ -1,37 +1,39 @@
 use axum::{
-    extract::{Query, State},
+    body::{Body, Bytes},
+    extract::{Path, Query, State},
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, post},
+    routing::{get, put},
     Json, Router,
 };
 use bombastic_index::Index;
 use bombastic_storage::{Config as StorageConfig, Storage};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-use std::path::Path;
 use std::sync::Arc;
-use std::sync::Mutex;
+use tokio::sync::{Mutex, RwLock};
 
-// TODO: Figure out a way to not lock since we use it read only
-type SharedState = Arc<Mutex<AppState>>;
-
-pub struct AppState {
-    index: Index,
-    storage: Storage,
+struct AppState {
+    storage: RwLock<Storage>,
+    // TODO: Figure out a way to not lock since we use it read only
+    index: Mutex<Index>,
 }
 
-pub async fn run<T: AsRef<Path>, B: Into<SocketAddr>>(
+type SharedState = Arc<AppState>;
+
+pub async fn run<T: AsRef<std::path::Path>, B: Into<SocketAddr>>(
     index: T,
     bind: B,
 ) -> Result<(), anyhow::Error> {
-    let storage = Storage::new(StorageConfig::new_minio_test())?;
-    let index = Index::new(index)?;
-    let state = Arc::new(Mutex::new(AppState { storage, index }));
+    let storage = RwLock::new(Storage::new(StorageConfig::new_minio_test())?);
+    let index = Mutex::new(Index::new(index)?);
+
+    let state = Arc::new(AppState { storage, index });
 
     let app = Router::new()
-        .route("/api/v1/sbom", get(fetch_sbom))
-        .route("/api/v1/sbom", post(publish_sbom))
+        .route("/api/v1/sbom", get(query_sbom))
+        .route("/api/v1/sbom/:id", get(fetch_sbom))
+        .route("/api/v1/sbom/:id", put(publish_sbom))
         .with_state(state);
 
     let addr = bind.into();
@@ -44,33 +46,48 @@ pub async fn run<T: AsRef<Path>, B: Into<SocketAddr>>(
 
 async fn fetch_sbom(
     State(state): State<SharedState>,
-    Query(params): Query<FetchParams>,
-) -> (StatusCode, Json<FetchResponse>) {
-    // TODO: Implement
-    (StatusCode::NOT_FOUND, Json(FetchResponse {}))
-}
-
-#[derive(Debug, Deserialize)]
-struct FetchParams {
-    purl: Option<String>,
-    sha256: Option<String>,
+    Path(id): Path<String>,
+) -> (StatusCode, Bytes) {
+    let storage = state.storage.read().await;
+    // TODO: Stream payload/SBOM directly from body rather than going via serde_json.
+    match storage.get(&id).await {
+        Ok(data) => (StatusCode::NOT_FOUND, data.into()),
+        Err(e) => (StatusCode::NOT_FOUND, "".into()),
+    }
 }
 
 #[derive(Serialize)]
 struct FetchResponse {}
 
-async fn publish_sbom(
+async fn query_sbom(
     State(state): State<SharedState>,
-    Json(payload): Json<PublishRequest>,
-) -> (StatusCode, Json<PublishResponse>) {
-    let response = PublishResponse {};
+    Query(params): Query<QueryParams>,
+) -> (StatusCode, Json<QueryResponse>) {
     // TODO: Implement
-    (StatusCode::CREATED, Json(response))
+    (StatusCode::NOT_FOUND, Json(QueryResponse {}))
 }
 
-#[derive(Deserialize)]
-struct PublishRequest {
-    username: String,
+#[derive(Debug, Deserialize)]
+struct QueryParams {
+    purl: Option<String>,
+    sha256: Option<String>,
+}
+
+#[derive(Serialize)]
+struct QueryResponse {}
+
+async fn publish_sbom(
+    State(state): State<SharedState>,
+    Path(id): Path<String>,
+    data: Bytes,
+) -> (StatusCode, Json<PublishResponse>) {
+    let response = PublishResponse {};
+    let storage = state.storage.write().await;
+    // TODO: unbuffered I/O
+    match storage.put(&id, &data[..]).await {
+        Ok(_) => (StatusCode::CREATED, Json(response)),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(response)),
+    }
 }
 
 #[derive(Serialize)]
