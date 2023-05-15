deeb41f9ed6ab27a9d573d32b9a357a26f0d0267
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2023-05-15 10:03:21 +0200

    more info to generate script

--- testdata/generate.sh
+++ testdata/generate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-NUM=$1
+START=$1
+END=$2
 TEMPLATE=template.json
 
-for i in $(seq 1 $NUM)
+for i in $(seq $START $END)
 do
 	P="pkg:maven/io.seedwing/seedwing-java-example@1.0.$i-SNAPSHOT?type=jar"
 	cat $TEMPLATE | sed -e "s,SECRET_PURL,$P,g" > tmp.json
+	echo "Publishing mysbom$i"
 	curl -s -X PUT -d@tmp.json http://localhost:8080/api/v1/sbom/mysbom$i
 	#curl -s -X PUT -d@tmp.json https://bombastic-api-bombastic.apps.sandbox.drogue.world/api/v1/sbom/mysbom$i
 done
