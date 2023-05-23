fbf05382f4902c5cb846a0dd21ceca85bda224c5
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2023-05-23 13:33:59 +0200

    chore: remove red hat-specifics

--- spog/model/src/pkg.rs
+++ spog/model/src/pkg.rs
@@ -7,13 +7,6 @@ use utoipa::ToSchema;
 #[schema(example = json!(Package {
     purl: Some("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6".to_string()), href: Some(format!("/api/package?purl={}", &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6"))),
     sbom: Some(format!("/api/package/sbom?purl={}", &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6"))),
-    trusted: Some(true),
-    trusted_versions: vec![PackageRef {
-        purl: "pkg:rpm/redhat/openssl@1.1.1k-7.el8_6".to_string(),
-        href: format!("/api/package?purl={}", &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6")),
-        trusted: Some(true),
-        sbom: Some(format!("/api/package/sbom?purl={}", &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6"))),
-    }],
     vulnerabilities: vec![VulnerabilityRef {
         cve: "cve-2023-0286".into(),
         href: "https://access.redhat.com/security/cve/cve-2023-0286".into()
@@ -47,13 +40,12 @@ pub struct VulnerabilityRef {
 #[schema(example = json!(PackageRef {
     purl: "pkg:rpm/redhat/openssl@1.1.1k-7.el8_6".to_string(),
     href: format!("/api/package?purl={}", &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6")),
-    trusted: Some(true),
     sbom: None
 }))]
 pub struct PackageRef {
     pub purl: String,
     pub href: String,
-    pub sbom: String,
+    pub sbom: Option<String>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, ToSchema, Serialize, Deserialize)]
@@ -64,7 +56,6 @@ pub struct SnykData;
     PackageRef {
         purl: "pkg:maven/io.vertx/vertx-web-common@4.3.7".to_string(),
         href: format!("/api/package?purl={}", &urlencoding::encode("pkg:maven/io.vertx/vertx-web-common@4.3.7")),
-        trusted: None,
         sbom: None,
     }
 ]))]
@@ -83,7 +74,6 @@ impl Deref for PackageDependencies {
     PackageRef {
         purl: "pkg:maven/io.quarkus/quarkus-vertx-http@2.16.2.Final".to_string(),
         href: format!("/api/package?purl={}", &urlencoding::encode("pkg:maven/io.quarkus/quarkus-vertx-http@2.16.2.Final")),
-        trusted: None,
         sbom: None,
     }
 ]))]
