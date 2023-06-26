3e92aac97913d1bf527db2d5ef116bf6726a0252
Jens Reimann <ctron@dentrassi.de>
2023-06-26 16:28:28 +0200

    refactor: add panic hook for SPOG UI

--- spog/ui/src/main.rs
+++ spog/ui/src/main.rs
@@ -10,6 +10,7 @@ mod model;
 mod pages;
 mod utils;
 
+use browser_panic_hook::{CustomBody, IntoPanicHook};
 use wasm_bindgen::prelude::*;
 
 #[cfg(not(debug_assertions))]
@@ -17,8 +18,39 @@ const LOG_LEVEL: log::Level = log::Level::Info;
 #[cfg(debug_assertions)]
 const LOG_LEVEL: log::Level = log::Level::Trace;
 
+fn set_panic_hook() {
+    yew::set_custom_panic_hook(
+        CustomBody(Box::new(|details| {
+            format!(
+                r#"
+<div class="pf-v5-l-bullseye">
+  <div class="pf-v5-l-bullseye__item">
+    <div class="pf-v5-c-alert pf-m-danger" aria-label="Application panicked">
+      <div class="pf-v5-c-alert__icon">
+        <i class="fas fa-fw fa-exclamation-circle" aria-hidden="true"></i>
+      </div>
+      <p class="pf-v5-c-alert__title">
+        <span class="pf-v5-screen-reader">Panick alert:</span>
+        Application panicked
+      </p>
+      <div class="pf-v5-c-alert__description">
+        <p>The application failed critically and cannot recover.</p>
+        <p>Reason: {message}</p>
+      </div>
+    </div>
+  </div>
+</div>
+"#,
+                message = details.message()
+            )
+        }))
+        .into_panic_hook(),
+    );
+}
+
 pub fn main() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::new(LOG_LEVEL));
+    set_panic_hook();
     yew::Renderer::<app::Application>::new().render();
     Ok(())
 }
