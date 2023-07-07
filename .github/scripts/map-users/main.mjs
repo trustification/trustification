c1c5ad80064ded178f354a4d7f23d53cbe9de071
Jens Reimann <ctron@dentrassi.de>
2023-07-07 16:18:39 +0200

    ci: allow using DMs too

--- .github/scripts/map-users/main.mjs
+++ .github/scripts/map-users/main.mjs
@@ -1,42 +1,87 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
-export default function message(payload) {
-  const mappingFile = new URL("../../matrix-users.yaml", import.meta.url);
-  const mappings = yaml.load(fs.readFileSync(mappingFile))?.mappings;
+export class Mapper {
+  constructor(payload) {
+    this.payload = payload;
 
-  let users = [];
-  let matrix_users = [];
-
-  for (let user of payload?.pull_request?.requested_reviewers ) {
-    // get user id
-    const login = user?.login;
-    if (login === undefined) {
-      continue;
-    }
-
-    // record for logging
-    users.push(login);
-
-    // map
-    let m = mappings?.[login];
-    if (m !== undefined) {
-      matrix_users.push(m);
-    }
+    const mappingFile = new URL("../../matrix-users.yaml", import.meta.url);
+    this.mappings = yaml.load(fs.readFileSync(mappingFile));
   }
 
-  console.log("Users:", users)
-  console.log("Matrix Users:", matrix_users)
+  requestedReviewers() {
+    let users = [];
 
-  if (!matrix_users.length) {
-    return undefined;
+    for (let user of this.payload?.pull_request?.requested_reviewers) {
+
+      const login = user?.login;
+      if (login === undefined) {
+        continue;
+      }
+
+      users.push(login);
+    }
+
+    return users;
   }
 
-  let s = (matrix_users.length > 1) ? "s" : "";
+  _formatMessage(matrixUsers) {
+    if (!matrixUsers.length) {
+      return undefined;
+    }
 
-  matrix_users = matrix_users.join(", ")
+    let s = (matrixUsers.length > 1) ? "s" : "";
 
-  return `Assigned ${matrix_users} as reviewer${s} for PR: ${payload?.pull_request?.html_url}`
+    matrixUsers = matrixUsers.join(", ")
+
+    return `Assigned ${matrixUsers} as reviewer${s} for PR: ${this.payload?.pull_request?.html_url}`
+  }
+
+  mapForChannel() {
+    let matrixUsers = [];
+
+    for (let user of this.requestedReviewers()) {
+      let m = this.mappings?.mapForChannel?.[user];
+      if (m !== undefined) {
+        matrixUsers.push(m);
+      }
+    }
+
+    return matrixUsers;
+  }
+
+  mapForDirectMessage () {
+    let matrixUsers = [];
+
+    for (let user of this.requestedReviewers()) {
+      let m = this.mappings?.mapForDirectMessage?.[user];
+      if (m !== undefined) {
+        matrixUsers.push(m);
+      }
+    }
+
+    return matrixUsers;
+  }
+
+  /// Return the message which should be sent to the channel (or none).
+  channelMessage () {
+    return this._formatMessage(this.mapForChannel());
+  }
+
+  /// Return the users receiving a direct message, as `--user <user>` arguments, ready to be passed to matrix-commander
+  directMessageArguments() {
+    return this.mapForDirectMessage().map(user => `--user "${user}"`).join(" ");
+  }
 }
 
+export function channelMessage(payload) {
+  const mapper = new Mapper(payload);
 
+  const users = mapper.requestedReviewers();
+  const channelUsers = mapper.mapForChannel();
+
+  console.log("Users:", users)
+  console.log("Matrix Users:", channelUsers)
+
+  return mapper.channelMessage();
+}
--- .github/scripts/map-users/test.mjs
+++ .github/scripts/map-users/test.mjs
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
-import message from "./main.mjs";
+import { channelMessage, Mapper } from "./main.mjs";
 
 const github = JSON.parse(fs.readFileSync('payload.json'))?.github;
 
-const output = message(github.event)
+const output = channelMessage(github.event)
 
 console.log("Output:", output)
+
+const args = new Mapper(github.event).directMessageArguments();
+console.log("Direct:", args)
+
