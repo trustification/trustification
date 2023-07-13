b96f2cef54889f5b3544c6b9ef63fe5fa33aa56b
Dejan Bosanac <dbosanac@redhat.com>
2023-07-13 12:50:08 +0200

    feat: add vex exporter

--- exporter/src/lib.rs
+++ exporter/src/lib.rs
@@ -18,11 +18,11 @@ pub enum Events {
 
 #[derive(clap::ValueEnum, Debug, Clone, Display)]
 #[strum(serialize_all = "lowercase")]
-pub enum Storage {
-    #[clap(name = "bombastic")]
-    Bombastic,
-    #[clap(name = "vexation")]
-    Vexation,
+pub enum DocumentType {
+    #[clap(name = "sbom")]
+    SBOM,
+    #[clap(name = "vex")]
+    VEX,
 }
 
 #[derive(clap::Args, Debug)]
@@ -36,11 +36,14 @@ pub struct Run {
     #[arg(long = "events", value_enum, default_value = "kafka")]
     pub(crate) events: Events,
 
-    #[arg(long = "stored-topic", default_value = "sbom-stored")]
-    pub(crate) stored_topic: String,
+    #[arg(long = "stored-topic")]
+    pub(crate) stored_topic: Option<String>,
 
     #[arg(long = "devmode", default_value_t = false)]
     pub(crate) devmode: bool,
+
+    #[arg(long = "document-type")]
+    pub(crate) document_type: DocumentType,
 
     #[command(flatten)]
     pub(crate) bus: EventBusConfig,
@@ -55,19 +58,30 @@ pub struct Run {
 impl Run {
     pub async fn run(mut self) -> anyhow::Result<ExitCode> {
         Infrastructure::from(self.infra)
-            .run("bombastic-exporter", |metrics| async move {
-                let default_bucket = self
-                    .storage
-                    .bucket
-                    .clone()
-                    .expect("Required parameter --storage-bucket not set");
-                let storage = self.storage.create(&default_bucket, self.devmode, metrics.registry())?;
+            .run("guac-exporter", |metrics| async move {
+                let (bucket, topic) = match self.document_type {
+                    DocumentType::SBOM => (
+                        self.storage.bucket.clone().unwrap_or("bombastic".into()),
+                        self.stored_topic.unwrap_or("sbom-stored".into()),
+                    ),
+                    DocumentType::VEX => (
+                        self.storage.bucket.clone().unwrap_or("vexination".into()),
+                        self.stored_topic.unwrap_or("vex-stored".into()),
+                    ),
+                };
+                log::info!(
+                    "Starting {} exporter using bucket '{}' and topic '{}'",
+                    self.document_type,
+                    bucket,
+                    topic
+                );
+                let storage = self.storage.create(&bucket, self.devmode, metrics.registry())?;
                 let bus = self.bus.create(metrics.registry()).await?;
                 let emitter = NatsEmitter::new(&self.guac_url).await?;
                 if self.devmode {
-                    bus.create(&[self.stored_topic.as_str()]).await?;
+                    bus.create(&[topic.as_str()]).await?;
                 }
-                exporter::run(storage, bus, emitter, self.stored_topic.as_str()).await
+                exporter::run(storage, bus, emitter, topic.as_str()).await
             })
             .await?;
         Ok(ExitCode::SUCCESS)
