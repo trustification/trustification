b15932ca1164014d6967c609394734710ec58980
Jens Reimann <ctron@dentrassi.de>
2023-07-18 08:47:03 +0200

    ci: fix failing prune script
    The script failed with:
    
    ```
    ./.github/prune-nightly.sh: line 10: [: 1687053248: unary operator expected
    ```
    
    First, adding `set -e` to ensure that things like this will be detected
    in the future.
    
    Second, wrap the variables in double quotes to ensure that the
    whitespaces in the entries of "TAGS" don't get split up by the shell,
    but are processed by `cut`.

--- .github/prune-nightly.sh
+++ .github/prune-nightly.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+
+set -e
+
 TAGS=$(git for-each-ref --sort=taggerdate --format '%(refname:short) %(taggerdate:short)' refs/tags | grep "nightly")
 NOW=$(date +%s)
 # 30 days = 24 * 3600 * 30
 OLDEST=$((NOW - 2592000))
+
 while read -r TAG; do
-    T=$(echo -n $TAG | cut -d ' ' -f 1)
-    D=$(echo -n $TAG | cut -d ' ' -f 2)
-    D=$(date -d $D +%s)
-    if [ $OLDEST -gt $D ]; then
-	git tag -d $T
-	git push origin :$T
+    T=$(echo -n "$TAG" | cut -d ' ' -f 1)
+    D=$(echo -n "$TAG" | cut -d ' ' -f 2)
+    D=$(date -d "$D" +%s)
+    if [ $OLDEST -gt "$D" ]; then
+      echo "Deleting tag: $T"
+      echo git tag -d "$T"
+      echo git push origin ":$T"
     fi
 done <<< "$TAGS"
