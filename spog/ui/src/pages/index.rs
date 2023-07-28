16d956b57e111419cfe353e0427351783f65bb3c
Jens Reimann <ctron@dentrassi.de>
2023-07-28 16:35:08 +0200

    feat: provide a unified search experience

--- spog/ui/src/pages/index.rs
+++ spog/ui/src/pages/index.rs
@@ -1,13 +1,40 @@
 use crate::hooks::use_config;
+use patternfly_yew::prelude::*;
 use yew::prelude::*;
 
 #[function_component(Index)]
 pub fn index() -> Html {
     let config = use_config();
 
+    let text = use_state_eq(String::new);
+    let onchange = use_callback(|new_text, text| text.set(new_text), text.clone());
+
     html!(
         <>
             { Html::from_html_unchecked(config.landing_page.content.clone().into()) }
+
+            <PageSection variant={PageSectionVariant::Light}>
+                <Bullseye>
+                    <InputGroup>
+                        <InputGroupItem>
+                            <TextInputGroup>
+                                <TextInputGroupMain
+                                    icon={Icon::Search}
+                                    value={(*text).clone()}
+                                    {onchange}
+                                />
+                            </TextInputGroup>
+                        </InputGroupItem>
+                        <InputGroupItem>
+                            <Button
+                                variant={ButtonVariant::Control}
+                                icon={Icon::ArrowRight}
+                            />
+                        </InputGroupItem>
+                    </InputGroup>
+                </Bullseye>
+            </PageSection>
+
         </>
     )
 }
