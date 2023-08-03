d94735214d42b66883db15a9d4f0319bbcd9f6a3
Jens Reimann <ctron@dentrassi.de>
2023-08-03 09:41:21 +0200

    chore: work towards unified search

--- spog/ui/src/pages/index.rs
+++ spog/ui/src/pages/index.rs
@@ -1,6 +1,8 @@
 use crate::hooks::use_config;
+use crate::pages::AppRoute;
 use patternfly_yew::prelude::*;
 use yew::prelude::*;
+use yew_nested_router::prelude::*;
 
 #[function_component(Index)]
 pub fn index() -> Html {
@@ -9,29 +11,56 @@ pub fn index() -> Html {
     let text = use_state_eq(String::new);
     let onchange = use_callback(|new_text, text| text.set(new_text), text.clone());
 
+    let router = use_router::<AppRoute>();
+    let onclick = use_callback(
+        |_, (router, terms)| {
+            if let Some(router) = router {
+                router.push(AppRoute::Search {
+                    terms: (**terms).clone(),
+                });
+            }
+        },
+        (router.clone(), text.clone()),
+    );
+    let onsubmit = use_callback(
+        |_, (router, terms)| {
+            if let Some(router) = router {
+                router.push(AppRoute::Search {
+                    terms: (**terms).clone(),
+                });
+            }
+        },
+        (router.clone(), text.clone()),
+    );
+
     html!(
         <>
             { Html::from_html_unchecked(config.landing_page.content.clone().into()) }
 
             <PageSection variant={PageSectionVariant::Light}>
                 <Bullseye>
-                    <InputGroup>
-                        <InputGroupItem>
-                            <TextInputGroup>
-                                <TextInputGroupMain
-                                    icon={Icon::Search}
-                                    value={(*text).clone()}
-                                    {onchange}
+                    <Form {onsubmit}>
+                        // needed to trigger submit when pressing enter in the search field
+                        <input type="submit" hidden=true formmethod="dialog" />
+                        <InputGroup>
+                            <InputGroupItem>
+                                <TextInputGroup>
+                                    <TextInputGroupMain
+                                        icon={Icon::Search}
+                                        value={(*text).clone()}
+                                        {onchange}
+                                    />
+                                </TextInputGroup>
+                            </InputGroupItem>
+                            <InputGroupItem>
+                                <Button
+                                    variant={ButtonVariant::Control}
+                                    icon={Icon::ArrowRight}
+                                    {onclick}
                                 />
-                            </TextInputGroup>
-                        </InputGroupItem>
-                        <InputGroupItem>
-                            <Button
-                                variant={ButtonVariant::Control}
-                                icon={Icon::ArrowRight}
-                            />
-                        </InputGroupItem>
-                    </InputGroup>
+                            </InputGroupItem>
+                        </InputGroup>
+                    </Form>
                 </Bullseye>
             </PageSection>
 
