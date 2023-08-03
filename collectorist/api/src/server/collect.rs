91d55d8f0dddad280e98e58967301a75d88402c4
Jim Crossley <jim@crossleys.org>
2023-08-03 17:24:57 -0400

    feat: plumb the collectorist api for triggering fan-out
    Returning a data stub for the moment
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- collectorist/api/src/server/collect.rs
+++ collectorist/api/src/server/collect.rs
@@ -8,20 +8,22 @@ pub struct CollectRequest {
     pub(crate) purls: Vec<String>,
 }
 
-/// Register a collector
+/// Post a list of purls to be "gathered"
 #[utoipa::path(
     post,
     tag = "collectorist",
     path = "/api/v1/collect",
     responses(
-        (status = 200, description = "Collector registered"),
-        (status = BAD_REQUEST, description = "Missing valid id"),
+        (status = 200, description = "Purls gathered"),
+        (status = BAD_REQUEST, description = "Malformed input"),
     ),
 )]
 #[post("/collect")]
 pub(crate) async fn collect(
-    _state: web::Data<SharedState>,
-    _req: web::Json<CollectRequest>,
+    state: web::Data<SharedState>,
+    input: web::Json<CollectRequest>,
 ) -> actix_web::Result<impl Responder> {
-    Ok(HttpResponse::Ok().finish())
+    let purls = input.into_inner();
+    let result = state.gatherer.gather(state.get_ref().clone(), purls).await;
+    Ok(HttpResponse::Ok().json(result))
 }
