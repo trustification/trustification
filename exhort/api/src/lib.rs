290d5b4a65e51ef4d2a1c69268657363f8c7b1fb
Jim Crossley <jim@crossleys.org>
2023-08-04 18:16:32 -0400

    feat: exhort analysis delegates to collectorist fanout
    Still need to flesh out the data according to CRDA requirements
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- exhort/api/src/lib.rs
+++ exhort/api/src/lib.rs
@@ -16,17 +16,20 @@ pub struct Run {
     #[arg(short, long, default_value = "0.0.0.0")]
     pub bind: String,
 
-    #[arg(short = 'p', long = "port", default_value_t = 9919)]
+    #[arg(short = 'p', long = "port", default_value_t = 9920)]
     pub port: u16,
 
     #[command(flatten)]
     pub infra: InfrastructureConfig,
+
+    #[arg(short = 'u', long = "collectorist-url", default_value = "http://localhost:9919/")]
+    pub(crate) collectorist_url: String,
 }
 
 impl Run {
     pub async fn run(self) -> anyhow::Result<ExitCode> {
         let infra = Infrastructure::from(self.infra).run("exhort-api", |_metrics| async move {
-            let state = Self::configure()?;
+            let state = Self::configure(self.collectorist_url)?;
             let addr = SocketAddr::from_str(&format!("{}:{}", self.bind, self.port))?;
 
             server::run(state, addr).await
@@ -37,13 +40,14 @@ impl Run {
         Ok(ExitCode::SUCCESS)
     }
 
-    fn configure() -> anyhow::Result<Arc<AppState>> {
-        let state = Arc::new(AppState {});
-
+    fn configure(collectorist_url: String) -> anyhow::Result<Arc<AppState>> {
+        let state = Arc::new(AppState { collectorist_url });
         Ok(state)
     }
 }
 
-pub struct AppState {}
+pub struct AppState {
+    collectorist_url: String,
+}
 
 pub(crate) type SharedState = Arc<AppState>;
