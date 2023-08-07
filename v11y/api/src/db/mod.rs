48d30fe194be5c0c1f17abedbf2170d5098495ce
Bob McWhirter <bmcwhirt@redhat.com>
2023-08-07 16:00:06 -0400

    Wire up v11y to the trust cli. Correct some issues creating db tables/indexes.

--- v11y/api/src/db/mod.rs
+++ v11y/api/src/db/mod.rs
@@ -35,6 +35,7 @@ impl Db {
     }
 
     async fn create_vulnerabilities_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'vulnerabilities'");
         sqlx::query(
             r#"create table if not exists vulnerabilities (
                     id text not null,
@@ -42,12 +43,13 @@ impl Db {
                     published datatime,
                     withdrawn datetime,
                     summary text,
-                    details text,
+                    details text
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'vulnerabilities_pk'");
         sqlx::query(
             r#"
             create unique index if not exists vulnerabilities_pk ON vulnerabilities ( id ) ;
@@ -60,16 +62,18 @@ impl Db {
     }
 
     async fn create_severities_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'severities'");
         sqlx::query(
             r#"create table if not exists severities (
                     vulnerability_id text not null,
                     type text,
-                    score text,
+                    score text
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'severities_pk'");
         sqlx::query(
             r#"
             create index if not exists severities_pk ON severities ( vulnerability_id ) ;
@@ -82,15 +86,17 @@ impl Db {
     }
 
     async fn create_aliases_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'aliases'");
         sqlx::query(
             r#"create table if not exists aliases (
                     vulnerability_id text not null,
-                    alias text not null,
+                    alias text not null
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'aliases_by_id'");
         sqlx::query(
             r#"
             create index if not exists alias_by_id ON aliases ( vulnerability_id ) ;
@@ -103,15 +109,17 @@ impl Db {
     }
 
     async fn create_related_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'related'");
         sqlx::query(
             r#"create table if not exists related (
                     vulnerability_id text not null,
-                    related text not null,
+                    related text not null
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'related_by_id'");
         sqlx::query(
             r#"
             create index if not exists related_by_id ON aliases ( vulnerability_id ) ;
@@ -124,15 +132,17 @@ impl Db {
     }
 
     async fn create_events_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'events'");
         sqlx::query(
             r#"create table if not exists events (
                     vulnerability_id text not null,
-                    event text not null,
+                    event text not null
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'events_by_id'");
         sqlx::query(
             r#"
             create index if not exists events_by_id ON events ( vulnerability_id ) ;
@@ -145,16 +155,18 @@ impl Db {
     }
 
     async fn create_references_table(&self) -> Result<(), anyhow::Error> {
+        log::debug!("create table 'references'");
         sqlx::query(
             r#"create table if not exists references (
                     vulnerability_id text not null,
                     type text not null,
-                    url text not null,
+                    url text not null
                 )"#,
         )
         .execute(&self.pool)
         .await?;
 
+        log::debug!("create index 'references_by_id'");
         sqlx::query(
             r#"
             create index if not exists references_by_id ON events ( vulnerability_id ) ;
--- v11y/v11y/src/lib.rs
+++ v11y/v11y/src/lib.rs
@@ -1,1 +1,14 @@
+use std::process::ExitCode;
 
+#[derive(clap::Subcommand, Debug)]
+pub enum Command {
+    Api(v11y_api::Run),
+}
+
+impl Command {
+    pub async fn run(self) -> anyhow::Result<ExitCode> {
+        match self {
+            Self::Api(run) => run.run().await,
+        }
+    }
+}
