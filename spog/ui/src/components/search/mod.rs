aae178f662f382026d656fa6606672fa4e83fe0a
Jens Reimann <ctron@dentrassi.de>
2023-08-07 11:19:05 +0200

    chore: more work in unified search

--- spog/ui/src/components/search/mod.rs
+++ spog/ui/src/components/search/mod.rs
@@ -7,3 +7,18 @@ pub use dynamic::*;
 pub use help::*;
 pub use simple::*;
 pub use toolbar::*;
+
+#[derive(Clone, PartialEq, Eq)]
+pub enum SearchPropertiesMode {
+    Managed { query: Option<String> },
+    Provided { terms: String },
+}
+
+impl SearchPropertiesMode {
+    pub fn props_query(&self) -> Option<String> {
+        match &self {
+            Self::Managed { query } => query.clone(),
+            _ => None,
+        }
+    }
+}
---
+++ spog/ui/src/utils/count.rs
@@ -0,0 +1,44 @@
+use patternfly_yew::prelude::*;
+use spog_model::search::SearchResult;
+use yew::prelude::*;
+use yew_more_hooks::prelude::UseAsyncState;
+
+pub trait ResultCount {
+    fn count_result(&self) -> Option<usize>;
+}
+
+impl<T> ResultCount for SearchResult<T> {
+    fn count_result(&self) -> Option<usize> {
+        self.total
+    }
+}
+
+impl<T, E> ResultCount for UseAsyncState<T, E>
+where
+    T: ResultCount,
+{
+    fn count_result(&self) -> Option<usize> {
+        match &*self {
+            UseAsyncState::Ready(Ok(data)) => data.count_result(),
+            _ => None,
+        }
+    }
+}
+
+pub fn count_tab_title<T, E>(title: impl AsRef<str>, state: &UseAsyncState<T, E>) -> Html
+where
+    T: ResultCount,
+{
+    let badge = match state {
+        UseAsyncState::Ready(Ok(data)) => match data.count_result() {
+            Some(count) => html!(<> {" "} <Badge> { count } </Badge> </>),
+            None => html!(),
+        },
+        UseAsyncState::Ready(Err(_)) => html!(),
+        _ => {
+            html!(<> {" "} <Badge read=true> { Icon::InProgress } </Badge></>)
+        }
+    };
+
+    html!(<> {title.as_ref()} { badge } </>)
+}
