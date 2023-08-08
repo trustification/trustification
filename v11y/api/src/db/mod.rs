0bcd62722ffb4d59b75e4075bf33c9e3f9ff3247
Bob McWhirter <bmcwhirt@redhat.com>
2023-08-08 15:19:05 -0400

    Adjust some of the DB schema, ingest minimal vulns.

--- v11y/api/src/db/mod.rs
+++ v11y/api/src/db/mod.rs
@@ -1,6 +1,11 @@
-use sqlx::sqlite::SqliteConnectOptions;
-use sqlx::SqlitePool;
+use futures::Stream;
+use futures::StreamExt;
 use std::str::FromStr;
+
+use sqlx::sqlite::SqliteConnectOptions;
+use sqlx::{Row, SqlitePool};
+
+use v11y_client::Vulnerability;
 
 static DB_FILE_NAME: &str = "v11y.db";
 
@@ -39,8 +44,9 @@ impl Db {
         sqlx::query(
             r#"create table if not exists vulnerabilities (
                     id text not null,
-                    modified datetime,
-                    published datatime,
+                    origin text not null,
+                    modified datetime not null,
+                    published datatime not null,
                     withdrawn datetime,
                     summary text,
                     details text
@@ -52,7 +58,7 @@ impl Db {
         log::debug!("create index 'vulnerabilities_pk'");
         sqlx::query(
             r#"
-            create unique index if not exists vulnerabilities_pk ON vulnerabilities ( id ) ;
+            create unique index if not exists vulnerabilities_pk ON vulnerabilities ( id, origin ) ;
             "#,
         )
         .execute(&self.pool)
@@ -174,6 +180,141 @@ impl Db {
         )
         .execute(&self.pool)
         .await?;
+
+        Ok(())
+    }
+
+    pub async fn ingest(&self, vuln: &Vulnerability) -> Result<(), anyhow::Error> {
+        sqlx::query(
+            r#"
+                insert or ignore into vulnerabilities (
+                    id, origin, modified, published, withdrawn, summary, details
+                ) values (
+                    $1, $2, $3, $4, $5, $6, $7
+                )
+            "#,
+        )
+        .bind(vuln.id.clone())
+        .bind(vuln.origin.clone())
+        .bind(vuln.modified.clone())
+        .bind(vuln.published.clone())
+        .bind(vuln.withdrawn.clone())
+        .bind(vuln.summary.clone())
+        .bind(vuln.details.clone())
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+
+    #[allow(unused)]
+    pub async fn get_known_ids(&self) -> impl Stream<Item = String> {
+        sqlx::query(r#"select distinct id from vulnerabilities"#)
+            .fetch(&self.pool)
+            .filter_map(|row| async move {
+                if let Ok(row) = row {
+                    Some(row.get::<String, _>("id"))
+                } else {
+                    None
+                }
+            })
+    }
+
+    #[allow(unused)]
+    pub async fn get_known_origins(&self) -> impl Stream<Item = String> {
+        sqlx::query(r#"select distinct origin from vulnerabilities"#)
+            .fetch(&self.pool)
+            .filter_map(|row| async move {
+                if let Ok(row) = row {
+                    Some(row.get::<String, _>("origin"))
+                } else {
+                    None
+                }
+            })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use futures::StreamExt;
+    use v11y_client::Vulnerability;
+
+    use crate::db::Db;
+
+    #[tokio::test]
+    async fn create_db() -> Result<(), anyhow::Error> {
+        let db = Db::new().await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn ingest_minimal() -> Result<(), anyhow::Error> {
+        let db = Db::new().await?;
+
+        let vuln = Vulnerability {
+            origin: "osv".to_string(),
+            id: "CVE-123".to_string(),
+            modified: "2023-08-08T18:17:02Z".parse()?,
+            published: "2023-08-08T18:17:02Z".parse()?,
+            withdrawn: None,
+            summary: "Summary".to_string(),
+            details: "Some\ndetails".to_string(),
+            aliases: vec![],
+            severities: vec![],
+            ranges: vec![],
+            related: vec![],
+            references: vec![],
+        };
+
+        db.ingest(&vuln).await?;
+
+        let vuln = Vulnerability {
+            origin: "snyk".to_string(),
+            id: "CVE-123".to_string(),
+            modified: "2023-08-08T18:17:02Z".parse()?,
+            published: "2023-08-08T18:17:02Z".parse()?,
+            withdrawn: None,
+            summary: "Summary".to_string(),
+            details: "Some\ndetails".to_string(),
+            aliases: vec![],
+            severities: vec![],
+            ranges: vec![],
+            related: vec![],
+            references: vec![],
+        };
+
+        db.ingest(&vuln).await?;
+
+        let vuln = Vulnerability {
+            origin: "osv".to_string(),
+            id: "CVE-345".to_string(),
+            modified: "2023-08-08T18:17:02Z".parse()?,
+            published: "2023-08-08T18:17:02Z".parse()?,
+            withdrawn: None,
+            summary: "Summary".to_string(),
+            details: "Some\ndetails".to_string(),
+            aliases: vec![],
+            severities: vec![],
+            ranges: vec![],
+            related: vec![],
+            references: vec![],
+        };
+
+        db.ingest(&vuln).await?;
+
+        let ids: Vec<_> = db.get_known_ids().await.collect().await;
+
+        println!("{:?}", ids);
+
+        assert_eq!(2, ids.len());
+        assert!(ids.contains(&"CVE-123".to_owned()));
+        assert!(ids.contains(&"CVE-345".to_owned()));
+
+        let origins: Vec<_> = db.get_known_origins().await.collect().await;
+
+        assert_eq!(2, origins.len());
+        assert!(origins.contains(&"snyk".to_owned()));
+        assert!(origins.contains(&"osv".to_owned()));
 
         Ok(())
     }
--- v11y/client/src/lib.rs
+++ v11y/client/src/lib.rs
@@ -1,1 +1,99 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Vulnerability {
+    pub origin: String,
+    pub id: String,
+    pub modified: DateTime<Utc>,
+    pub published: DateTime<Utc>,
+    pub withdrawn: Option<DateTime<Utc>>,
+    pub summary: String,
+    pub details: String,
+    #[serde(skip_serializing_if = "Vec::is_empty", default = "Vec::default")]
+    pub aliases: Vec<String>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default = "Vec::default")]
+    pub severities: Vec<Severity>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default = "Vec::default")]
+    pub ranges: Vec<Range>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default = "Vec::default")]
+    pub related: Vec<String>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default = "Vec::default")]
+    pub references: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Severity {
+    r#type: ScoreType,
+    score: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Range {
+    lower: Option<Version>,
+    upper: Option<Version>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Version {
+    Inclusive(String),
+    Exclusive(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ScoreType {
+    Cvss3,
+    Cvss4,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Event {
+    r#type: EventType,
+    event: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum EventType {
+    Introduced,
+    Fixed,
+}
+
+pub struct V11yClient {
+    url: String,
+}
+
+impl V11yClient {
+    pub fn new(url: &str) -> Self {
+        Self { url: url.to_owned() }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Vulnerability;
+
+    #[tokio::test]
+    async fn serialization() -> Result<(), anyhow::Error> {
+        let json = r#"
+            {
+                "origin": "osv",
+                "id": "CVE-123",
+                "modified": "2023-08-08T18:17:02Z",
+                "published": "2023-08-08T18:17:02Z",
+                "summary": "This is my summary",
+                "details": "And\nhere are some\ndetails",
+                "related": [
+                    "related-foo",
+                    "related-bar"
+                ]
+            }
+        "#;
+
+        let vuln: Vulnerability = serde_json::from_str(json)?;
+
+        assert_eq!("osv", vuln.origin);
+        assert_eq!("CVE-123", vuln.id);
+
+        Ok(())
+    }
+}
