532203a4d01b466b07bf0c2ce011abe249a74c33
Dejan Bosanac <dbosanac@redhat.com>
2023-08-16 12:44:29 +0200

    chore: refine Guac errors

--- spog/api/src/services/guac.rs
+++ spog/api/src/services/guac.rs
@@ -1,5 +1,5 @@
 use actix_web::{http::header::ContentType, HttpResponse};
-use guac::client::GuacClient;
+use guac::client::{Error as GuacError, GuacClient};
 use http::StatusCode;
 use spog_model::prelude::{PackageDependencies, PackageDependents, PackageList};
 use trustification_common::error::ErrorInformation;
@@ -12,21 +12,41 @@ pub struct GuacService {
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Guac error: {0}")]
-    Guac(#[source] anyhow::Error),
+    Guac(#[from] GuacError),
 }
 
 impl actix_web::error::ResponseError for Error {
     fn error_response(&self) -> HttpResponse {
         let mut res = HttpResponse::build(self.status_code());
         res.insert_header(ContentType::json());
-        res.json(ErrorInformation {
-            error: format!("{}", self.status_code()),
-            message: "Error constructing url to backend service".to_string(),
-            details: self.to_string(),
-        })
+        match self {
+            Self::Guac(error) => match error {
+                GuacError::Purl(err) => res.json(ErrorInformation {
+                    error: format!("{}", self.status_code()),
+                    message: "Purl parsing error".to_string(),
+                    details: err.to_string(),
+                }),
+                GuacError::Http(err) => res.json(ErrorInformation {
+                    error: format!("{}", self.status_code()),
+                    message: "Purl parsing error".to_string(),
+                    details: err.to_string(),
+                }),
+                GuacError::GraphQL(msg) => res.json(ErrorInformation {
+                    error: format!("{}", self.status_code()),
+                    message: "Purl parsing error".to_string(),
+                    details: msg.to_string(),
+                }),
+            },
+        }
     }
+
     fn status_code(&self) -> StatusCode {
-        StatusCode::INTERNAL_SERVER_ERROR
+        match self {
+            Self::Guac(error) => match error {
+                GuacError::Purl(_) => StatusCode::BAD_REQUEST,
+                _ => StatusCode::INTERNAL_SERVER_ERROR,
+            },
+        }
     }
 }
 
@@ -39,19 +59,19 @@ impl GuacService {
 
     /// Lookup related packages for a provided Package URL
     pub async fn get_packages(&self, purl: &str) -> Result<PackageList, Error> {
-        let pkgs = self.client.get_packages(purl).await.map_err(Error::Guac)?;
+        let pkgs = self.client.get_packages(purl).await?;
         Ok(PackageList::from(pkgs))
     }
 
     /// Lookup dependencies for a provided Package URL
     pub async fn get_dependencies(&self, purl: &str) -> Result<PackageDependencies, Error> {
-        let deps = self.client.is_dependency(purl).await.map_err(Error::Guac)?;
+        let deps = self.client.is_dependency(purl).await?;
         Ok(PackageDependencies::from(deps))
     }
 
     /// Lookup dependents for a provided Package URL
     pub async fn get_dependents(&self, purl: &str) -> Result<PackageDependents, Error> {
-        let deps = self.client.is_dependent(purl).await.map_err(Error::Guac)?;
+        let deps = self.client.is_dependent(purl).await?;
         Ok(PackageDependents::from(deps))
     }
 }
