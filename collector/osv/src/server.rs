a6e390a7ebb4b2d0db0330d0c0a625fc3ef40d77
Bob McWhirter <bmcwhirt@redhat.com>
2023-08-29 13:28:54 -0400

    Rev guac-rs client and adjust API usage for GuacService.

--- collector/osv/src/server.rs
+++ collector/osv/src/server.rs
@@ -1,15 +1,17 @@
 use std::fmt::Debug;
 use std::net::{SocketAddr, TcpListener};
+use std::str::FromStr;
 use std::sync::atomic::Ordering;
 use std::time::Duration;
 
 use actix_web::middleware::{Compress, Logger};
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder, ResponseError};
 use derive_more::Display;
-use guac::client::certify_vuln::Metadata;
+use guac::client::intrinsic::certify_vuln::ScanMetadataInput;
+use guac::client::intrinsic::vulnerability::VulnerabilityInputSpec;
 use guac::client::GuacClient;
-use guac::client::vulnerability::Vulnerability;
 use log::{info, warn};
+use packageurl::PackageUrl;
 use reqwest::Url;
 use tokio::time::sleep;
 use utoipa::OpenApi;
@@ -34,6 +36,9 @@ pub enum Error {
 
     #[display(fmt = "OSV error")]
     OsvError,
+
+    #[display(fmt = "Internal error")]
+    InternalError,
 }
 
 impl ResponseError for Error {}
@@ -102,7 +107,7 @@ pub async fn collect_packages(
         .cloned()
         .ok_or(Error::Configuration)?;
 
-    let guac = GuacClient::new(guac_url.to_string());
+    let guac = GuacClient::new(guac_url.as_str());
     let request: QueryBatchRequest = (&*request).into();
     //log::debug!("osv request: {}", serde_json::to_string_pretty(&request).unwrap());
     let response = OsvClient::query_batch(request).await.map_err(|_| Error::OsvError)?;
@@ -110,29 +115,38 @@ pub async fn collect_packages(
     for entry in &response.results {
         if let Some(vulns) = &entry.vulns {
             if let Package::Purl { purl } = &entry.package {
-                guac.ingest_package(purl).await.map_err(|_| Error::GuacError)?;
-                for vuln in vulns {
-                    guac.ingest_vulnerability("osv", &vuln.id)
-                        .await
-                        .map_err(|_| Error::GuacError)?;
-                    guac.ingest_certify_vuln(
-                        purl,
-                        Vulnerability {
-                            ty: "osv".to_string(),
-                            vulnerability_id: vuln.id.clone(),
-                        },
-                        Metadata {
-                            db_uri: "https://osv.dev/".to_string(),
-                            db_version: "1.0".to_string(),
-                            scanner_uri: "https://trustification.io/".to_string(),
-                            scanner_version: "1.0".to_string(),
-                            time_scanned: Default::default(),
-                            origin: "osv".to_string(),
-                            collector: "osv".to_string(),
-                        },
-                    )
+                guac.intrinsic()
+                    .ingest_package(&PackageUrl::from_str(purl).map_err(|_| Error::InternalError)?.into())
                     .await
                     .map_err(|_| Error::GuacError)?;
+                for vuln in vulns {
+                    guac.intrinsic()
+                        //.ingest_vulnerability("osv", &vuln.id)
+                        .ingest_vulnerability(&VulnerabilityInputSpec {
+                            r#type: "osv".to_string(),
+                            vulnerability_id: vuln.id.clone(),
+                        })
+                        .await
+                        .map_err(|_| Error::GuacError)?;
+                    guac.intrinsic()
+                        .ingest_certify_vuln(
+                            &PackageUrl::from_str(purl).map_err(|_| Error::InternalError)?.into(),
+                            &VulnerabilityInputSpec {
+                                r#type: "osv".to_string(),
+                                vulnerability_id: vuln.id.clone(),
+                            },
+                            &ScanMetadataInput {
+                                db_uri: "https://osv.dev/".to_string(),
+                                db_version: "1.0".to_string(),
+                                scanner_uri: "https://trustification.io/".to_string(),
+                                scanner_version: "1.0".to_string(),
+                                time_scanned: Default::default(),
+                                origin: "osv".to_string(),
+                                collector: "osv".to_string(),
+                            },
+                        )
+                        .await
+                        .map_err(|_| Error::GuacError)?;
                 }
             }
         }
