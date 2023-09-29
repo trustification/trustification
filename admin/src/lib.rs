2aa13c5bd10821109bb114b423f09a256a84ab97
Ulf Lilleengen <lulf@redhat.com>
2023-09-29 13:21:31 +0200

    task: add admin delete command for deleting documents
    The delete command can delete SBOM or VEX documents by their id, a regular expression or simply deleting all.

--- admin/src/lib.rs
+++ admin/src/lib.rs
@@ -1,17 +1,24 @@
 use std::process::ExitCode;
 
+mod delete;
 mod reindex;
 
 #[derive(clap::Subcommand, Debug)]
 pub enum Command {
     #[command(subcommand)]
     Reindex(reindex::Reindex),
+    #[command(subcommand)]
+    Delete(delete::Delete),
 }
 
 impl Command {
     pub async fn run(self) -> anyhow::Result<ExitCode> {
+        if let Err(e) = env_logger::builder().format_timestamp_millis().try_init() {
+            eprintln!("Error initializing logging: {:?}", e);
+        }
         match self {
             Self::Reindex(reindex) => reindex.run().await,
+            Self::Delete(delete) => delete.run().await,
         }
     }
 }
