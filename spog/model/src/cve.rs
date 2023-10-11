dade8f9aa97c4617aa523256a4644bd0088ca387
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-10-11 15:57:13 +0200

    Add related products table

--- spog/model/src/cve.rs
+++ spog/model/src/cve.rs
@@ -3,12 +3,37 @@ use std::collections::BTreeMap;
 #[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct CveDetails {
     pub id: String,
-    pub products: BTreeMap<String, Vec<String>>,
+    pub products: BTreeMap<ProductCveStatus, Vec<ProductRelatedToCve>>,
     pub advisories: Vec<AdvisoryOverview>,
 
     #[serde(default)]
     pub details: Vec<v11y_model::Vulnerability>,
 }
+
+#[derive(Clone, Debug, PartialEq, Eq, Ord, PartialOrd, serde::Deserialize, serde::Serialize)]
+pub enum ProductCveStatus {
+    Fixed,
+    FirstFixed,
+    FirstAffected,
+    KnownAffected,
+    LastAffected,
+    KnownNotAffected,
+    Recommended,
+    UnderInvestigation,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+pub struct ProductRelatedToCve {
+    pub sbom_id: String,
+    pub packages: Vec<PackageRelatedToProductCve>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+pub struct PackageRelatedToProductCve {
+    pub purl: String,
+    pub r#type: String,
+}
+
 
 #[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub struct AdvisoryOverview {
