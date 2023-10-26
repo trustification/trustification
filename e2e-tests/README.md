f5935a99c88534d3445c2972d0e0a6e4ebdd30c8
mrrajan <rravi@redhat.com>
2023-10-26 19:55:01 +0530

    Added Two scenarios
    Signed-off-by: mrrajan <rravi@redhat.com>

---
+++ e2e-tests/README.md
@@ -0,0 +1,22 @@
+# End - End UI Tests
+### Overview
+The End - End UI framework is based on [Cucumber](https://github.com/cucumber-rs/cucumber "Cucumber") and [Thrityfour](https://github.com/stevepryde/thirtyfour/tree/main/thirtyfour "Thrityfour"). The tests are written in Gherkin format and they are executed with the aid of cucumber-rs. 
+The tests under package e2e-tests can be run with the below command,
+```shell
+git clone https://github.com/trustification/trustification
+cd e2e-tests 
+cargo run -- --application http://localhost:8084/ --user-name admin --password admin123456```
+####Usage
+Clone the Trustification repository using the provided URL.
+Navigate to the e2e-tests directory.
+Execute the tests using cargo run with the following command-line arguments:
+- `--application:` The URL of the application under test.
+- `--user-name:` The username for logging into the application.
+- `--password:`   The password for logging into the application.
+
+### References
+For more information, you can refer to the following resources:
+
+1. [Cucumber](https://github.com/cucumber-rs/cucumber "Cucumber"): Cucumber for Rust, the framework used for writing and running tests in Gherkin format.
+2.  [Thirtyfour](https://github.com/stevepryde/thirtyfour/tree/main/thirtyfour "Thirtyfour"): Thirtyfour is a Selenium WebDriver library for Rust, which is used for browser automation in your UI tests.
+These references provide detailed information about the tools and libraries used in your End-to-End UI testing framework.
--- e2e-tests/src/main.rs
+++ e2e-tests/src/main.rs
@@ -1,16 +1,15 @@
-
-mod world;
 mod pages;
-use cucumber::{cli, World, writer, WriterExt};
-use thirtyfour::{Capabilities, WebDriver, DesiredCapabilities};
-use world::{E2EContext, E2EWorld};
+mod world;
+use cucumber::{cli, writer, World, WriterExt};
 use futures::FutureExt;
 use std::fs::File;
-use std::sync::Arc;
 use std::process::Command;
+use std::sync::Arc;
+use thirtyfour::{DesiredCapabilities, WebDriver};
+use world::{E2EContext, E2EWorld};
 
-#[derive(cli::Args, Clone)]  // re-export of `clap::Args`
-pub struct E2Ecliparser{
+#[derive(cli::Args, Clone)] // re-export of `clap::Args`
+pub struct E2Ecliparser {
     ///Application URL under test
     #[arg(long)]
     pub application: String,
@@ -20,9 +19,9 @@ pub struct E2Ecliparser{
     ///Password to login to Application
     #[arg(long)]
     pub password: String,
-    ///Select (chrome/firefox) to run the Application
-    #[arg(long)]
-    pub browser: String,
+    /////Select (chrome/firefox) to run the Application
+    //#[arg(long)]
+    //pub browser: String,
     /////Select (chrome/firefox) to run the Application
     //#[arg(long)]
     //pub browsercap: String,
@@ -30,41 +29,39 @@ pub struct E2Ecliparser{
 
 #[tokio::main]
 async fn main() {
-    let junit_output_file = File::create(format!("{}/junit.xml", env!("CARGO_MANIFEST_DIR"))).expect("Error file creation");
-    let json_output_file = File::create(format!("{}/cucumber.json", env!("CARGO_MANIFEST_DIR"))).expect("Error file creation");
+    let junit_output_file =
+        File::create(format!("{}/junit.xml", env!("CARGO_MANIFEST_DIR"))).expect("Error file creation");
+    let json_output_file =
+        File::create(format!("{}/cucumber.json", env!("CARGO_MANIFEST_DIR"))).expect("Error file creation");
     let opts = cli::Opts::<_, _, _, E2Ecliparser>::parsed();
     let _option = opts.custom.clone();
     let opts_custom = Arc::new(_option);
     E2EWorld::cucumber()
         .before(move |_, _, _, world| {
+            {
                 let opts_custom = Arc::clone(&opts_custom);
                 let mut context = E2EContext::new();
                 Box::pin(async move {
-                    let mut serverurl = "http://localhost:9515";
-                    let mut caps: Capabilities = DesiredCapabilities::chrome().into();
-                    if opts_custom.browser.as_str() == "firefox"{
-                        caps = DesiredCapabilities::firefox().into();
-                        serverurl = "http://localhost:4444";
-                        Command::new("geckodriver")
-                                .args(["--port=4444"])
-                                .spawn()
-                                .expect("Failed to execute process");
-                    }else{
-                        Command::new("chromedriver")
-                                .args(["--port=9515"])
-                                .spawn()
-                                .expect("Failed to execute process");
-                    };
-                    let driver = WebDriver::new(serverurl, caps).await.expect("Error while creating Webdriver");
+                    let mut caps = DesiredCapabilities::chrome();
+                    let serverurl = "http://localhost:9515";
+                    caps.add_chrome_arg("--window-size=1920,1080")
+                        .expect("Window size error");
+                    Command::new("chromedriver")
+                        .args(["--port=9515"])
+                        .spawn()
+                        .expect("Failed to execute process");
+                    let driver = WebDriver::new(serverurl, caps)
+                        .await
+                        .expect("Error while creating Webdriver");
                     context.insert(driver);
                     world.context = Arc::new(context);
                     world.application = Some(opts_custom.application.clone());
-                    world.browser = Some(opts_custom.browser.clone());
                     world.user_name = Some(opts_custom.user_name.clone());
                     world.password = Some(opts_custom.password.clone());
                 })
             }
-        .boxed_local())
+            .boxed_local()
+        })
         .with_cli(opts)
         .after(move |_, _, _, _, world| {
             async move {
@@ -75,15 +72,12 @@ async fn main() {
                 }
             }
             .boxed_local()
-        }) 
+        })
         .with_writer(
             writer::Libtest::or_basic()
-                .tee::<E2EWorld, _>(writer::JUnit::for_tee(
-                    junit_output_file,
-                    writer::Verbosity::default(),
-                ))
+                .tee::<E2EWorld, _>(writer::JUnit::for_tee(junit_output_file, writer::Verbosity::default()))
                 .tee::<E2EWorld, _>(writer::Json::for_tee(json_output_file))
-                .normalized()
+                .normalized(),
         )
         .run("tests/features/")
         .await;
