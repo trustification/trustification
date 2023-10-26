79a757330f171c27131b83970ed01bc10776a96e
Xaiofeng Bai <bxf12315@gmail.com>
2023-10-26 22:57:03 +0800

    init part of use case 3

---
+++ spog/api/src/package.rs
@@ -0,0 +1,124 @@
+use crate::search;
+// use crate::server::AppState;
+// use crate::service::v11y::V11yService;
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse,
+};
+use spog_model::package_Info::{PackageInfo, V11yRef};
+use spog_model::search::PackageInfoSummary;
+use std::sync::Arc;
+use trustification_api::search::{SearchOptions, SearchResult};
+use trustification_auth::authenticator::Authenticator;
+use trustification_infrastructure::new_auth;
+
+pub(crate) fn configure(auth: Option<Arc<Authenticator>>) -> impl FnOnce(&mut ServiceConfig) {
+    |config: &mut ServiceConfig| {
+        config.service(
+            web::scope("/api/v1/package_info")
+                .wrap(new_auth!(auth))
+                .service(web::resource("/search").to(packages_search_mock)), // .service(web::resource("/{id}").to(cve_get))
+                                                                             // .service(web::resource("/{id}/related-products").to(cve_related_product)),
+        );
+    }
+}
+
+#[utoipa::path(
+get,
+path = "/api/v1/package_info/search",
+responses(
+(status = 200, description = "packages was found"),
+(status = NOT_FOUND, description = "packages was not found")
+),
+params(
+("id" = String, Path, description = "Id of advisory to fetch"),
+)
+)]
+
+pub async fn packages_search_mock(
+    web::Query(params): web::Query<search::QueryParams>,
+) -> actix_web::Result<HttpResponse> {
+    let pkgs = make_mock_data();
+    let mut result = SearchResult::<Vec<PackageInfo>> {
+        total: Some(pkgs.len()),
+        result: pkgs,
+    };
+    Ok(HttpResponse::Ok().json(result))
+}
+
+fn make_mock_data() -> Vec<PackageInfo> {
+    let mut packages = vec![
+        PackageInfo {
+            name: "io.quarkus.arc:arc".to_string().into(),
+            version: "2.16.2.Final".to_string().into(),
+            package_type: "maven".to_string().into(),
+            description: "case one".to_string().into(),
+            purl: "pkg:maven/io.quarkus.arc/arc@2.16.2.Final?type=jar".to_string().into(),
+            href: Some(format!(
+                "/api/package?purl={}",
+                &urlencoding::encode("pkg:maven/io.quarkus.arc/arc@2.16.2.Final?type=jar")
+            )),
+            sbom: Some(format!(
+                "/api/package/sbom?purl={}",
+                &urlencoding::encode("pkg:maven/io.quarkus.arc/arc@2.16.2.Final?type=jar")
+            )),
+            supplier: "Organization: Red Hat".to_string().into(),
+            vulnerabilities: vec![
+                V11yRef {
+                    cve: "cve-2023-0286".to_string().into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "low".to_string(),
+                },
+                V11yRef {
+                    cve: "cve-2023-0286".to_string().into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "medium".to_string(),
+                },
+                V11yRef {
+                    cve: "cve-2023-0286".to_string().into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "high".to_string(),
+                },
+                V11yRef {
+                    cve: "cve-2023-0286".to_string().into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "critical".to_string(),
+                },
+            ],
+        },
+        PackageInfo {
+            name: "redhat:openssl".to_string().into(),
+            version: "1.1.1k-7.el8_6".to_string().into(),
+            package_type: "rpm".to_string().into(),
+            description: "case two".to_string().into(),
+            purl: Some("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6".to_string()),
+            href: Some(format!(
+                "/api/package?purl={}",
+                &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6")
+            )),
+            sbom: Some(format!(
+                "/api/package/sbom?purl={}",
+                &urlencoding::encode("pkg:rpm/redhat/openssl@1.1.1k-7.el8_6")
+            )),
+            supplier: "Organization: Red Hat".to_string().into(),
+            vulnerabilities: vec![
+                V11yRef {
+                    cve: "cve-2023-0286".into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "low".to_string(),
+                },
+                V11yRef {
+                    cve: "cve-2023-0286".into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "medium".to_string(),
+                },
+                V11yRef {
+                    cve: "cve-2023-0286".into(),
+                    href: "https://access.redhat.com/security/cve/cve-2023-0286".into(),
+                    severity: "critical".to_string(),
+                },
+            ],
+        },
+    ];
+    packages
+}
---
+++ spog/ui/crates/backend/src/package_info.rs
@@ -0,0 +1,41 @@
+use crate::{ApplyAccessToken, Backend, Endpoint, SearchParameters};
+use spog_model::prelude::*;
+use spog_ui_common::error::*;
+use std::rc::Rc;
+use trustification_api::search::SearchResult;
+use trustification_api::Apply;
+use yew_oauth2::prelude::*;
+
+pub struct PackageInfoService {
+    backend: Rc<Backend>,
+    access_token: Option<LatestAccessToken>,
+    client: reqwest::Client,
+}
+
+#[allow(unused)]
+impl PackageInfoService {
+    pub fn new(backend: Rc<Backend>, access_token: Option<LatestAccessToken>) -> Self {
+        Self {
+            backend,
+            access_token,
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn search_packages(
+        &self,
+        q: &str,
+        options: &SearchParameters,
+    ) -> Result<SearchResult<Vec<PackageInfoSummary>>, Error> {
+        let response = self
+            .client
+            .get(self.backend.join(Endpoint::Api, "/api/v1/package_info/search")?)
+            .query(&[("q", q)])
+            .apply(options)
+            .latest_access_token(&self.access_token)
+            .send()
+            .await?;
+        log::warn!("pub async fn search_packages");
+        Ok(response.error_for_status()?.json().await?)
+    }
+}
