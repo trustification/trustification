08bf8180936f2591d237eb8e220880119e6cdca0
Jens Reimann <ctron@dentrassi.de>
2023-10-26 10:31:34 +0200

    refactor(spog): replace custom logic with `find_vulnerability`

--- spog/api/src/utils/spdx.rs
+++ spog/api/src/utils/spdx.rs
@@ -3,6 +3,7 @@ use spdx_rs::models::SPDX;
 /// find all PURLs in the package information
 ///
 /// This walks through all package information entries, returning a tuple of (purl, id).
+#[allow(unused)]
 pub fn find_purls(spdx: &SPDX) -> impl Iterator<Item = (&str, &str)> {
     spdx.package_information.iter().flat_map(|pi| {
         pi.external_reference
