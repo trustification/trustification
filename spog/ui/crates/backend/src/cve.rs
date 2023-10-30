995ef409b66bd6805d238c4076c3e4a90dfade66
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-10-30 11:30:18 +0100

    Add Vulnerabilities details page

--- spog/ui/crates/backend/src/cve.rs
+++ spog/ui/crates/backend/src/cve.rs
@@ -39,6 +39,21 @@ impl CveService {
         Ok(response.api_error_for_status().await?.json().await?)
     }
 
+    pub async fn get_from_index(&self, id: &str) -> Result<SearchResult<Vec<CveSearchDocument>>, Error> {
+        let q = format!("id:{id}");
+        let response = self
+            .client
+            .get(self.backend.join(Endpoint::Api, "/api/v1/cve")?)
+            .query(&[("q", q)])
+            .latest_access_token(&self.access_token)
+            .send()
+            .await?;
+
+        let result: SearchResult<Vec<SearchHit<CveSearchDocument>>> = response.error_for_status()?.json().await?;
+
+        Ok(result.map(|result| result.into_iter().map(|result| result.document).collect()))
+    }
+
     pub async fn get_related_products(&self, id: impl AsRef<str>) -> Result<CveDetails, ApiError> {
         let url = self.backend.join(
             Endpoint::Api,
