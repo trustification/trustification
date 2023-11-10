e23bf61b372a99b4f7627c7dac19102778897748
Xaiofeng Bai <bxf12315@gmail.com>
2023-11-10 10:19:55 +0800

    let metrics support prefix

--- infrastructure/src/infra.rs
+++ infrastructure/src/infra.rs
@@ -257,6 +257,10 @@ impl Metrics {
     pub fn registry(&self) -> &Registry {
         &self.registry
     }
+
+    pub fn custom_registry(&self, prefix: String) -> Registry {
+        Registry::new_custom(prefix.into(), Option::None).unwrap()
+    }
 }
 
 pub struct InfrastructureRunner {
