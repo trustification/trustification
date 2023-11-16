bbacd3711ad0326b37603a1c92a22239d80fc0fb
Jens Reimann <ctron@dentrassi.de>
2023-11-16 14:20:28 +0100

    chore: adapt to new API

--- spog/api/src/service/guac.rs
+++ spog/api/src/service/guac.rs
@@ -216,7 +216,7 @@ impl GuacService {
     /// Later on, this will be replaced with the SPDX namespace, or the CycloneDX serial.
     ///
     /// The result is `map<cve, set<purls>>`.
-    #[instrument(skip(self), ret, err)]
+    #[instrument(skip(self), err)]
     pub async fn find_vulnerability(
         &self,
         id: GuacSbomIdentifier<'_>,
@@ -231,6 +231,29 @@ impl GuacService {
         log::debug!("Using GUAC purl: {purl}");
 
         Ok(self.client.semantic().find_vulnerability(&purl, offset, limit).await?)
+    }
+
+    /// Find vulnerabilities for an SBOM (by SBOM UID)
+    ///
+    /// The `sbom_id` is an identifier for an SBOM. Currently, this is a special PURL format of
+    /// `pkg:/guac/pkg/<name>@<version>`, where `<name>` and `<version>` are coming from the single
+    /// (expected) "document describes" component.
+    ///
+    /// Later on, this will be replaced with the SPDX namespace, or the CycloneDX serial.
+    ///
+    /// The result is `map<cve, set<purls>>`.
+    #[instrument(skip(self), err)]
+    pub async fn find_vulnerability_by_uid(
+        &self,
+        id: &str,
+        offset: Option<i64>,
+        limit: Option<i64>,
+    ) -> Result<HashMap<String, BTreeSet<String>>, Error> {
+        Ok(self
+            .client
+            .semantic()
+            .find_vulnerability_by_sbom_uri(id, offset, limit)
+            .await?)
     }
 }
 
