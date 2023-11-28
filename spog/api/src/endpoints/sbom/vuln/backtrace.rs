27e6ef6979c26ff215f48eee91d87d1f14e23c29
Jens Reimann <ctron@dentrassi.de>
2023-11-28 11:43:10 +0100

    refactor: remove mock data

--- spog/api/src/endpoints/sbom/vuln/backtrace.rs
+++ spog/api/src/endpoints/sbom/vuln/backtrace.rs
@@ -1,36 +1,18 @@
 use super::Error;
 use crate::service::guac::GuacService;
-use names::Generator;
 use packageurl::PackageUrl;
-use rand::Rng;
 use spog_model::prelude::Backtrace;
 use tracing::instrument;
 
 /// take a PURL, a retrieve all paths towards the main entry point of its SBOM
-// FIXME: This needs to be implemented
 #[instrument(skip(_guac), err)]
 pub async fn backtrace<'a>(
     _guac: &GuacService,
     _purl: &'a PackageUrl<'a>,
 ) -> Result<impl Iterator<Item = Backtrace> + 'a, Error> {
-    let mut rng = rand::thread_rng();
-    let mut names = Generator::default();
-
-    // create some mock data
     let mut result = vec![];
 
-    for _ in 0..rng.gen_range(0..5) {
-        let mut trace = vec![];
-        for _ in 0..rng.gen_range(1..5) {
-            trace.push(
-                PackageUrl::new("mock", names.next().unwrap_or_else(|| "mock".to_string()))
-                    .map(|purl| purl.to_string())
-                    .unwrap_or_else(|_| "pkg://mock/failed".to_string())
-                    .to_string(),
-            );
-        }
-        result.push(Backtrace(trace));
-    }
+    // FIXME: This needs to be implemented
 
     Ok(result.into_iter())
 }
