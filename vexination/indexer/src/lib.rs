10ef357458ede83494aeab81d69dd685c7038d9e
Jim Crossley <jim@crossleys.org>
2024-01-27 16:17:52 -0500

    fix: point vex indexer at bombastic topic and uniquify group name
    Debugging this was a real PITA!!!
    
    I still need to figure out what to do with all the bucket/topic
    configuration for the now defunkt vexination -- not easy to test all
    the config changes, obviously.
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- vexination/indexer/src/lib.rs
+++ vexination/indexer/src/lib.rs
@@ -59,7 +59,7 @@ impl Run {
                         IndexStore::new(&self.storage, &self.index, index, context.metrics.registry())
                     })?;
                     let storage = Storage::new(
-                        storage.process("vexination", self.devmode),
+                        storage.process("bombastic", self.devmode),
                         Validator::VEX,
                         context.metrics.registry(),
                     )?;
