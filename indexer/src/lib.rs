ca081ee97bc9163706f9aefb878bfad666e2690b
Jim Crossley <jim@crossleys.org>
2024-01-29 13:07:54 -0500

    Revert "fix: point vex indexer at bombastic topic and uniquify group name"
    This reverts commit 6e971e26b79ab956a642336115c632d857857c0f.
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- indexer/src/lib.rs
+++ indexer/src/lib.rs
@@ -91,10 +91,7 @@ where
         for index in &mut self.indexes {
             writers.push(block_in_place(|| index.writer())?);
         }
-        let consumer = self
-            .bus
-            .subscribe(&format!("indexer-{}", self.storage.name()), &[self.stored_topic])
-            .await?;
+        let consumer = self.bus.subscribe("indexer", &[self.stored_topic]).await?;
         let mut processed_events = Vec::new();
         let mut indexed_events = Vec::new();
         let mut events = 0;
@@ -338,7 +335,7 @@ where
     ) -> Result<(), anyhow::Error> {
         match block_in_place(|| writer.add_document(index, key, data)) {
             Ok(_) => {
-                log::debug!("Inserted entry '{key}' into index '{}'", index.name());
+                log::debug!("Inserted entry '{key}' into index");
             }
             Err(e) => {
                 let failure = serde_json::json!( {
