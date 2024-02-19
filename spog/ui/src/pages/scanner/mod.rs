32be80c9b977da636626b53fb19c0f4a95a03c63
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2024-02-19 13:28:09 +0100

    Remove Cyclonedx 1.3 from page description

--- spog/ui/src/pages/scanner/mod.rs
+++ spog/ui/src/pages/scanner/mod.rs
@@ -189,7 +189,7 @@ fn common_header(props: &CommonHeaderProperties) -> Html {
                     <Content>
                         <Title>{"Scan an SBOM"}</Title>
                         <p>
-                            {"Load an existing CycloneDX 1.3, 1.4 or SPDX 2.2 file"}
+                            {"Load an existing CycloneDX 1.4 or SPDX 2.2 file"}
                             if let Some(url) = &config.scanner.documentation_url {
                                 {" or "}
                                 <a
