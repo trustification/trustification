c541a3a8f5ffa07e9618015c2c7315090c9e5f40
Jens Reimann <ctron@dentrassi.de>
2024-03-01 13:10:02 +0100

    feat: add report to vexination walker

--- vexination/walker/src/report.rs
+++ vexination/walker/src/report.rs
@@ -1,40 +1,42 @@
 use async_trait::async_trait;
-use sbom_walker::{
+use csaf_walker::{
     retrieve::RetrievalError,
-    validation::{ValidatedSbom, ValidatedVisitor, ValidationContext, ValidationError},
+    validation::{ValidatedAdvisory, ValidatedVisitor, ValidationError},
 };
 use trustification_common_walker::report::{Phase, ReportVisitor, Severity};
 use walker_common::utils::url::Urlify;
-use walker_extras::visitors::{SendValidatedSbomError, SendVisitor};
+use walker_extras::visitors::{SendValidatedAdvisoryError, SendVisitor};
 
-pub struct SbomReportVisitor(pub ReportVisitor);
+pub struct AdvisoryReportVisitor(pub ReportVisitor);
 
 #[async_trait(?Send)]
-impl ValidatedVisitor for SbomReportVisitor {
+impl ValidatedVisitor for AdvisoryReportVisitor {
     type Error = <SendVisitor as ValidatedVisitor>::Error;
     type Context = <SendVisitor as ValidatedVisitor>::Context;
 
-    async fn visit_context(&self, context: &ValidationContext) -> Result<Self::Context, Self::Error> {
+    async fn visit_context(
+        &self,
+        context: &csaf_walker::validation::ValidationContext,
+    ) -> Result<Self::Context, Self::Error> {
         self.0.next.visit_context(context).await
     }
 
-    async fn visit_sbom(
+    async fn visit_advisory(
         &self,
         context: &Self::Context,
-        result: Result<ValidatedSbom, ValidationError>,
+        result: Result<ValidatedAdvisory, ValidationError>,
     ) -> Result<(), Self::Error> {
         let file = result.url().to_string();
 
         self.0.report.lock().tick();
 
-        let result = self.0.next.visit_sbom(context, result).await;
+        let result = self.0.next.visit_advisory(context, result).await;
 
         if let Err(err) = &result {
             match err {
-                SendValidatedSbomError::Validation(ValidationError::Retrieval(RetrievalError::InvalidResponse {
-                    code,
-                    ..
-                })) => {
+                SendValidatedAdvisoryError::Validation(ValidationError::Retrieval(
+                    RetrievalError::InvalidResponse { code, .. },
+                )) => {
                     self.0.report.lock().add_error(
                         Phase::Retrieval,
                         file,
@@ -48,7 +50,9 @@ impl ValidatedVisitor for SbomReportVisitor {
                         return Ok(());
                     }
                 }
-                SendValidatedSbomError::Validation(ValidationError::DigestMismatch { expected, actual, .. }) => {
+                SendValidatedAdvisoryError::Validation(ValidationError::DigestMismatch {
+                    expected, actual, ..
+                }) => {
                     self.0.report.lock().add_error(
                         Phase::Validation,
                         file,
@@ -60,7 +64,7 @@ impl ValidatedVisitor for SbomReportVisitor {
                     // current file. Once it gets updated, we can reprocess it.
                     return Ok(());
                 }
-                SendValidatedSbomError::Validation(ValidationError::Signature { error, .. }) => {
+                SendValidatedAdvisoryError::Validation(ValidationError::Signature { error, .. }) => {
                     self.0.report.lock().add_error(
                         Phase::Validation,
                         file,
@@ -71,7 +75,7 @@ impl ValidatedVisitor for SbomReportVisitor {
                     // If there's a signature error, we can't do much other than ignoring the
                     // current file. Once it gets updated, we can reprocess it.
                 }
-                SendValidatedSbomError::Store(err) => {
+                SendValidatedAdvisoryError::Store(err) => {
                     self.0.report.lock().add_error(
                         Phase::Upload,
                         file,
