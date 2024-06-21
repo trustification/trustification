3af7dbb1aef0507ec5c882cdfac1e4c04eb1f882
Jiri Kucera <jkucera@redhat.com>
2024-06-21 11:31:56 +0200

    tests: A bit of tests refactoring
    * use `RequestFactory` to reduce code similarities
    * fix improper use of `encode` in queries (`RequestBuilder::query`
      calls `encode` automatically)
    * use consistent naming and comments style
    
    Signed-off-by: Jiří Kučera <jkucera@redhat.com>

--- integration-tests/tests/spog.rs
+++ integration-tests/tests/spog.rs
@@ -1,26 +1,25 @@
 #![allow(clippy::unwrap_used)]
 
-use integration_tests::{id, SpogContext, Urlifier};
+use integration_tests::{id, RequestFactory, SpogContext};
 use reqwest::StatusCode;
 use serde_json::{json, Value};
 use std::time::Duration;
 use test_context::test_context;
-use trustification_auth::client::TokenInjector;
 
 #[test_context(SpogContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
 async fn spog_version(context: &mut SpogContext) {
-    let response = reqwest::Client::new()
-        .get(context.urlify("/.well-known/trustification/version"))
-        .inject_token(&context.provider.provider_user)
+    let version: Value = RequestFactory::<&[(&str, &str)], Value>::new()
+        .with_provider_user()
+        .get("/.well-known/trustification/version")
+        .expect_status(StatusCode::OK)
+        .send(context)
         .await
+        .1
         .unwrap()
-        .send()
-        .await
+        .try_into()
         .unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let version: Value = response.json().await.unwrap();
     assert_eq!(version["name"], "spog-api");
 }
 
@@ -31,16 +30,16 @@ async fn spog_endpoints(context: &mut SpogContext) {
     let vexination_url = String::from(context.vexination.url.as_str());
     let bombastic_url = String::from(context.bombastic.url.as_str());
 
-    let response = reqwest::Client::new()
-        .get(context.urlify("/.well-known/trustification/endpoints"))
-        .inject_token(&context.provider.provider_user)
+    let endpoints: Value = RequestFactory::<&[(&str, &str)], Value>::new()
+        .with_provider_user()
+        .get("/.well-known/trustification/endpoints")
+        .expect_status(StatusCode::OK)
+        .send(context)
         .await
+        .1
         .unwrap()
-        .send()
-        .await
+        .try_into()
         .unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let endpoints: Value = response.json().await.unwrap();
     assert_eq!(endpoints["vexination"], vexination_url);
     assert_eq!(endpoints["bombastic"], bombastic_url);
 }
@@ -52,33 +51,17 @@ async fn spog_endpoints(context: &mut SpogContext) {
 #[tokio::test]
 #[ntest::timeout(60_000)]
 async fn spog_search_forward_bombastic(context: &mut SpogContext) {
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(context.urlify("/api/v1/sbom/search"))
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
-
+    let request = RequestFactory::<_, Value>::new()
+        .with_provider_user()
+        .get("/api/v1/sbom/search");
+    request.clone().expect_status(StatusCode::OK).send(context).await;
     // Check forwarding of search errors
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(context.urlify("/api/v1/sbom/search"))
-        .query(&[("q", urlencoding::encode("unknown:field"))])
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    request
+        .clone()
+        .with_query(&[("q", "unknown:field")])
+        .expect_status(StatusCode::BAD_REQUEST)
+        .send(context)
+        .await;
 }
 
 /// SPoG is the entrypoint for the frontend. It exposes a search API, but forwards requests
@@ -88,18 +71,12 @@ async fn spog_search_forward_bombastic(context: &mut SpogContext) {
 #[tokio::test]
 #[ntest::timeout(60_000)]
 async fn spog_search_forward_vexination(context: &mut SpogContext) {
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(context.urlify("/api/v1/advisory/search"))
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
+    RequestFactory::<&[(&str, &str)], Value>::new()
+        .with_provider_user()
+        .get("/api/v1/advisory/search")
+        .expect_status(StatusCode::OK)
+        .send(context)
+        .await;
 }
 
 #[test_with::env(CRDA_URL)]
@@ -107,26 +84,16 @@ async fn spog_search_forward_vexination(context: &mut SpogContext) {
 #[tokio::test]
 #[ntest::timeout(60_000)]
 async fn spog_crda_integration(context: &mut SpogContext) {
-    let client = reqwest::Client::new();
-
     let sbom = include_bytes!("crda/test1.sbom.json");
-
-    let response = client
-        .post(context.urlify("/api/v1/analyze/report"))
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .body(sbom.as_slice())
-        .send()
-        .await
-        .unwrap();
-
-    let status = response.status();
-    println!("Response: {}", status);
-    let html = response.text().await;
-    println!("{html:#?}");
-
-    assert_eq!(status, StatusCode::OK);
+    RequestFactory::<&[(&str, &str)], Value>::new()
+        .set_log_level(log::Level::Info)
+        .with_provider_user()
+        .post("/api/v1/analyze/report")
+        .with_body(sbom.as_slice())
+        .expect_status(StatusCode::OK)
+        .as_html()
+        .send(context)
+        .await;
 }
 
 /// SPoG API might enrich results from package search with related vulnerabilities. This test checks that this
@@ -135,40 +102,35 @@ async fn spog_crda_integration(context: &mut SpogContext) {
 #[tokio::test]
 #[ntest::timeout(120_000)]
 async fn spog_search_correlation(context: &mut SpogContext) {
-    let mut sbom: serde_json::Value = serde_json::from_str(include_str!("testdata/correlation/stf-1.5.json")).unwrap();
+    let mut sbom: Value = serde_json::from_str(include_str!("testdata/correlation/stf-1.5.json")).unwrap();
     let cpe_id = id("cpe:/a:redhat:service_telemetry_framework:1.5::el8");
     sbom["packages"][883]["externalRefs"][0]["referenceLocator"] = json!(cpe_id);
     let sbom_id = id("test-search-correlation");
     context.bombastic.upload_sbom(&sbom_id, &sbom).await;
 
-    let mut vex: serde_json::Value =
-        serde_json::from_str(include_str!("testdata/correlation/rhsa-2023_1529.json")).unwrap();
+    let mut vex: Value = serde_json::from_str(include_str!("testdata/correlation/rhsa-2023_1529.json")).unwrap();
     let vex_id = id("test-search-correlation");
     vex["document"]["tracking"]["id"] = json!(vex_id);
     vex["product_tree"]["branches"][0]["branches"][0]["branches"][0]["product"]["product_identification_helper"]
         ["cpe"] = json!(cpe_id);
     context.vexination.upload_vex(&vex).await;
 
-    let client = reqwest::Client::new();
+    let query = &[("q", format!("id:{sbom_id}"))];
+    let request = RequestFactory::<_, Value>::new()
+        .with_provider_user()
+        .get("/api/v1/sbom/search")
+        .with_query(query)
+        .expect_status(StatusCode::OK);
     // Ensure we can search for the data. We want to allow the
     // indexer time to do its thing, so might need to retry
     loop {
-        let response = client
-            .get(context.urlify(format!("/api/v1/sbom/search?q=id%3A{sbom_id}")))
-            .inject_token(&context.provider.provider_user)
-            .await
-            .unwrap()
-            .send()
-            .await
-            .unwrap();
-        assert_eq!(response.status(), StatusCode::OK, "unexpected status from search");
-        let payload: Value = response.json().await.unwrap();
+        let payload: Value = request.clone().send(context).await.1.unwrap().try_into().unwrap();
         if payload["total"].as_u64().unwrap() >= 1 {
             assert_eq!(payload["result"][0]["name"], json!("stf-1.5"), "unexpected sbom name");
 
             let data: spog_model::search::SbomSummary = serde_json::from_value(payload["result"][0].clone()).unwrap();
-            // println!("Data: {:?}", data);
-            // we need to have some information
+            log::trace!("Data: {:#?}", data);
+            // We need to have some information
             assert!(data.advisories.is_some(), "missing advisories");
             // Data might not be available until vex index is synced
             let advisories = data.advisories.unwrap();
@@ -191,60 +153,40 @@ async fn spog_dependencies(context: &mut SpogContext) {
     let sbom_id = "test-stf-1.5-guac";
     context.bombastic.upload_sbom(sbom_id, &input).await;
 
-    let client = reqwest::Client::new();
-
-    let purl: &str = "pkg:rpm/redhat/json-c@0.13.1-0.4.el8";
-
-    loop {
-        let response = client
-            .get(context.urlify("/api/v1/package/related"))
-            .query(&[("purl", purl)])
-            .inject_token(&context.provider.provider_user)
-            .await
-            .unwrap()
-            .send()
-            .await
-            .unwrap();
-        log::info!("{:#?}", response);
-        if response.status() == StatusCode::OK {
-            let payload: Value = response.json().await.unwrap();
-            log::info!("{:#?}", payload);
-            let pkgs = payload.as_array().unwrap();
-            if pkgs.contains(&json!({"purl": "pkg:rpm/redhat/json-c@0.13.1-0.4.el8?arch=x86_64"})) {
-                break;
+    let request = RequestFactory::<_, Value>::new()
+        .with_provider_user()
+        .test_status(StatusCode::OK);
+    for (purl, endpoint, expected_purl) in &[
+        (
+            "pkg:rpm/redhat/json-c@0.13.1-0.4.el8",
+            "/api/v1/package/related",
+            "pkg:rpm/redhat/json-c@0.13.1-0.4.el8?arch=x86_64",
+        ),
+        (
+            "pkg:rpm/redhat/json-c@0.13.1-0.4.el8",
+            "/api/v1/package/dependents",
+            "pkg:oci/registry.redhat.io/stf/sg-core@sha256:fae8586bc4872450e0f01f99a6202deec63ebd6b2ea8eb5c3f280229fa176da2?tag=5.1.1-3",
+        ),
+        (
+            "pkg:rpm/redhat/python-zope-event@4.2.0-9.2.el8stf",
+            "/api/v1/package/dependencies",
+            "pkg:rpm/redhat/python2-zope-event@4.2.0-9.2.el8stf?arch=noarch",
+        ),
+    ] {
+        loop {
+            let response = request.clone()
+                .get(endpoint)
+                .with_query(&[("purl", purl)])
+                .send(context)
+                .await;
+            if let (true, result) = response {
+                let payload: Value = result.unwrap().try_into().unwrap();
+                let data = payload.as_array().unwrap();
+                if data.contains(&json!({"purl": expected_purl})) {
+                    break;
+                }
             }
+            tokio::time::sleep(Duration::from_secs(1)).await;
         }
-        tokio::time::sleep(Duration::from_secs(1)).await;
     }
-
-    let response = client
-        .get(context.urlify("/api/v1/package/dependents"))
-        .query(&[("purl", purl)])
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let payload: Value = response.json().await.unwrap();
-    let deps = payload.as_array().unwrap();
-    log::info!("{:#?}", deps);
-    assert!(deps.contains(&json!({"purl": "pkg:oci/registry.redhat.io/stf/sg-core@sha256:fae8586bc4872450e0f01f99a6202deec63ebd6b2ea8eb5c3f280229fa176da2?tag=5.1.1-3"})));
-
-    let purl: &str = "pkg:rpm/redhat/python-zope-event@4.2.0-9.2.el8stf";
-    let response = client
-        .get(context.urlify("/api/v1/package/dependencies"))
-        .query(&[("purl", purl)])
-        .inject_token(&context.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::OK);
-    let payload: Value = response.json().await.unwrap();
-    let deps = payload.as_array().unwrap();
-    log::info!("{:#?}", deps);
-    assert!(deps.contains(&json!({"purl": "pkg:rpm/redhat/python2-zope-event@4.2.0-9.2.el8stf?arch=noarch"})));
 }
--- integration-tests/tests/vexination.rs
+++ integration-tests/tests/vexination.rs
@@ -1,52 +1,47 @@
 #![allow(clippy::unwrap_used)]
 
-use integration_tests::{get_response, id, Urlifier, VexinationContext};
+use integration_tests::{
+    get_response, id, FileUtility, FixtureKind, HasPushFixture, PayloadKind, RequestFactory, VexinationContext,
+};
 use reqwest::StatusCode;
 use serde_json::{json, Value};
 use std::time::Duration;
 use test_context::test_context;
-use tokio::fs::{remove_file, File};
-use trustification_auth::client::TokenInjector;
 use urlencoding::encode;
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn vexination_roundtrip(vexination: &mut VexinationContext) {
-    let client = reqwest::Client::new();
+async fn vexination_roundtrip(context: &mut VexinationContext) {
     let input = serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_1441.json")).unwrap();
-    vexination.upload_vex(&input).await;
+    context.upload_vex(&input).await;
     let id = input["document"]["tracking"]["id"].as_str().unwrap();
-    let advisory = encode(id);
 
     loop {
         // 1. Check that we can get the VEX back
-        let response = client
-            .get(vexination.urlify(format!("/api/v1/vex?advisory={advisory}")))
-            .header("Accept", "application/json")
-            .inject_token(&vexination.provider.provider_manager)
-            .await
-            .unwrap()
-            .send()
-            .await
-            .unwrap();
-
-        if response.status() == StatusCode::OK {
-            let body: Value = response.json().await.unwrap();
-            assert_eq!(body, input);
+        let response = RequestFactory::<_, Value>::new()
+            .with_provider_manager()
+            .get("/api/v1/vex")
+            .with_query(&[("advisory", &id)])
+            .with_headers(&[("Accept", "application/json")])
+            .test_status(StatusCode::OK)
+            .send(context)
+            .await;
+        if let (true, body) = response {
+            assert_eq!(<PayloadKind as TryInto<Value>>::try_into(body.unwrap()).unwrap(), input);
 
             // 2. Make sure we can search for the VEX (takes some time)
-            let response = client
-                .get(vexination.urlify(format!("/api/v1/vex/search?q=%22{advisory}%22")))
-                .inject_token(&vexination.provider.provider_manager)
+            let payload: Value = RequestFactory::<_, Value>::new()
+                .with_provider_manager()
+                .get("/api/v1/vex/search")
+                .with_query(&[("q", format!("\"{id}\""))])
+                .expect_status(StatusCode::OK)
+                .send(context)
                 .await
+                .1
                 .unwrap()
-                .send()
-                .await
+                .try_into()
                 .unwrap();
-
-            assert_eq!(response.status(), StatusCode::OK);
-            let payload: Value = response.json().await.unwrap();
 
             if payload["total"].as_u64().unwrap() >= 1 {
                 assert_eq!(id, payload["result"][0]["document"]["advisory_id"].as_str().unwrap());
@@ -62,38 +57,32 @@ async fn vexination_roundtrip(vexination: &mut VexinationContext) {
 #[ntest::timeout(30_000)]
 async fn vex_bad_search_queries(context: &mut VexinationContext) {
     // Ensure get expected errors on bad queries
+    let request = RequestFactory::<_, Value>::new()
+        .with_provider_manager()
+        .get("/api/v1/vex/search")
+        .expect_status(StatusCode::BAD_REQUEST);
     for query in &["unknown:foo", "foo sort:unknown"] {
-        let response = reqwest::Client::new()
-            .get(context.urlify(format!("/api/v1/vex/search?q={}", encode(query))))
-            .inject_token(&context.provider.provider_manager)
-            .await
-            .unwrap()
-            .send()
-            .await
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+        request.clone().with_query(&[("q", query)]).send(context).await;
     }
 }
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn upload_existing_vex_without_change(vexination: &mut VexinationContext) {
-    let mut input: serde_json::Value =
-        serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_3408.json")).unwrap();
+async fn upload_existing_vex_without_change(context: &mut VexinationContext) {
+    let mut input: Value = serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_3408.json")).unwrap();
 
-    // we generate a unique id and use it as the VEX's id for searching
+    // We generate a unique id and use it as the VEX's id for searching
     let key = id("test-unchanged-vex");
     input["document"]["tracking"]["id"] = json!(key);
 
     let id = encode(&key);
-    let url = vexination.urlify(format!("api/v1/vex?advisory={id}"));
-    vexination.upload_vex(&input).await;
-    let response1: serde_json::Value = get_response(&url, StatusCode::OK, &vexination.provider).await.into();
+    let url = format!("api/v1/vex?advisory={id}");
+    context.upload_vex(&input).await;
+    let response1: Value = get_response(context, &url, StatusCode::OK).await.into();
     assert_eq!(input, response1, "Content mismatch between request and response");
-    vexination.upload_vex(&input).await;
-    let response2: serde_json::Value = get_response(&url, StatusCode::OK, &vexination.provider).await.into();
+    context.upload_vex(&input).await;
+    let response2: Value = get_response(context, &url, StatusCode::OK).await.into();
     assert_eq!(
         input, response2,
         "Content mismatch between request and response after update"
@@ -103,22 +92,21 @@ async fn upload_existing_vex_without_change(vexination: &mut VexinationContext) 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn upload_existing_vex_with_change(vexination: &mut VexinationContext) {
-    let mut input: serde_json::Value =
-        serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2021_3029.json")).unwrap();
+async fn upload_existing_vex_with_change(context: &mut VexinationContext) {
+    let mut input: Value = serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2021_3029.json")).unwrap();
 
-    // we generate a unique id and use it as the VEX's id for searching
+    // We generate a unique id and use it as the VEX's id for searching
     let key = id("test-changed-vex");
     input["document"]["tracking"]["id"] = json!(key);
 
     let id = encode(&key);
-    let url = vexination.urlify(format!("api/v1/vex?advisory={id}"));
-    vexination.upload_vex(&input).await;
-    let response1: serde_json::Value = get_response(&url, StatusCode::OK, &vexination.provider).await.into();
+    let url = format!("api/v1/vex?advisory={id}");
+    context.upload_vex(&input).await;
+    let response1: Value = get_response(context, &url, StatusCode::OK).await.into();
     assert_eq!(input, response1, "Content mismatch between request and response");
     input["document"]["title"] = Value::String(String::from("Red Hat Vex Title Updated"));
-    vexination.upload_vex(&input).await;
-    let response2: serde_json::Value = get_response(&url, StatusCode::OK, &vexination.provider).await.into();
+    context.upload_vex(&input).await;
+    let response2: Value = get_response(context, &url, StatusCode::OK).await.into();
     assert_eq!(
         input, response2,
         "Content mismatch between request and response after update"
@@ -128,36 +116,31 @@ async fn upload_existing_vex_with_change(vexination: &mut VexinationContext) {
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn vex_invalid_type(vexination: &mut VexinationContext) {
-    let response = reqwest::Client::new()
-        .post(vexination.urlify("/api/v1/vex?id=foo"))
-        .body("<foo/>")
-        .header("Content-Type", "application/xml")
-        .inject_token(&vexination.provider.provider_manager)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+async fn vex_invalid_type(context: &mut VexinationContext) {
+    RequestFactory::<_, Value>::new()
+        .with_provider_manager()
+        .post("/api/v1/vex")
+        .with_query(&[("id", "foo")])
+        .with_headers(&[("Content-Type", "application/xml")])
+        .with_body(b"<foo/>".as_slice())
+        .expect_status(StatusCode::BAD_REQUEST)
+        .send(context)
+        .await;
 }
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn vex_invalid_encoding(vexination: &mut VexinationContext) {
-    let response = reqwest::Client::new()
-        .post(vexination.urlify("/api/v1/vex?id=foo"))
-        .body("{}")
-        .header("Content-Type", "application/json")
-        .header("Content-Encoding", "braille")
-        .inject_token(&vexination.provider.provider_manager)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+async fn vex_invalid_encoding(context: &mut VexinationContext) {
+    RequestFactory::<_, Value>::new()
+        .with_provider_manager()
+        .post("/api/v1/vex")
+        .with_query(&[("id", "foo")])
+        .with_headers(&[("Content-Type", "application/json"), ("Content-Encoding", "braille")])
+        .with_body(b"{}".as_slice())
+        .expect_status(StatusCode::BAD_REQUEST)
+        .send(context)
+        .await;
 }
 
 #[cfg(feature = "admin")]
@@ -167,90 +150,80 @@ async fn vex_invalid_encoding(vexination: &mut VexinationContext) {
 async fn upload_vex_empty_json(context: &mut VexinationContext) {
     let id = "test-empty-file-json";
     let input = serde_json::json!({});
-    let response = reqwest::Client::new()
-        .post(context.urlify(format!("/api/v1/vex?advisory={id}")))
-        .json(&input)
-        .inject_token(&context.provider.provider_manager)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-    context.delete_vex(id).await;
+    context.push_fixture(FixtureKind::Id(String::from(id)));
+    RequestFactory::new()
+        .with_provider_manager()
+        .post("/api/v1/vex")
+        .with_query(&[("advisory", id)])
+        .with_json(&input)
+        .expect_status(StatusCode::BAD_REQUEST)
+        .send(context)
+        .await;
 }
 
 #[cfg(feature = "admin")]
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(90_000)]
-async fn upload_vex_empty_file(vexination: &mut VexinationContext) {
+async fn upload_vex_empty_file(context: &mut VexinationContext) {
     let id = "test-empty-file-upload";
     let file_path = "empty-test.txt";
-    let _ = File::create(&file_path).await.expect("file creation failed");
-    let file = File::open(&file_path).await.unwrap();
-    let response = reqwest::Client::new()
-        .post(vexination.urlify(format!("/api/v1/vex?advisory={id}")))
-        .body(file)
-        .inject_token(&vexination.provider.provider_manager)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    remove_file(&file_path).await.unwrap();
-    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-    vexination.delete_vex(id).await;
+    let file = context.create_file(file_path).await;
+    context.push_fixture(FixtureKind::Id(String::from(id)));
+    RequestFactory::<_, Value>::new()
+        .with_provider_manager()
+        .post("/api/v1/vex")
+        .with_query(&[("advisory", id)])
+        .with_body(&file)
+        .expect_status(StatusCode::BAD_REQUEST)
+        .send(context)
+        .await;
 }
 
 #[cfg(feature = "admin")]
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn vex_upload_user_not_allowed(vexination: &mut VexinationContext) {
-    let input: serde_json::Value =
-        serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_1441.json")).unwrap();
-    let id = encode(input["document"]["tracking"]["id"].as_str().unwrap());
-    let response = reqwest::Client::new()
-        .post(vexination.urlify(format!("/api/v1/vex?advisory={id}")))
-        .json(&input)
-        .inject_token(&vexination.provider.provider_user)
-        .await
-        .unwrap()
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::FORBIDDEN);
+async fn vex_upload_user_not_allowed(context: &mut VexinationContext) {
+    let input: Value = serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_1441.json")).unwrap();
+    let id = input["document"]["tracking"]["id"].as_str().unwrap();
+    RequestFactory::new()
+        .with_provider_user()
+        .post("/api/v1/vex")
+        .with_query(&[("advisory", id)])
+        .with_json(&input)
+        .expect_status(StatusCode::FORBIDDEN)
+        .send(context)
+        .await;
 }
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn vex_upload_unauthorized(vexination: &mut VexinationContext) {
-    let input: serde_json::Value =
-        serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_1441.json")).unwrap();
-    let id = encode(input["document"]["tracking"]["id"].as_str().unwrap());
-    let response = reqwest::Client::new()
-        .post(vexination.urlify(format!("/api/v1/vex?advisory={id}")))
-        .json(&input)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+async fn vex_upload_unauthorized(context: &mut VexinationContext) {
+    let input: Value = serde_json::from_str(include_str!("../../vexination/testdata/rhsa-2023_1441.json")).unwrap();
+    let id = input["document"]["tracking"]["id"].as_str().unwrap();
+    RequestFactory::new()
+        .post("/api/v1/vex")
+        .with_query(&[("advisory", id)])
+        .with_json(&input)
+        .expect_status(StatusCode::UNAUTHORIZED)
+        .send(context)
+        .await;
 }
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn get_vex_with_missing_qualifier(vexination: &mut VexinationContext) {
-    let api_endpoint = vexination.urlify("api/v1/vex?id=missing_qualifier");
-    get_response(&api_endpoint, StatusCode::BAD_REQUEST, &vexination.provider).await;
+async fn get_vex_with_missing_qualifier(context: &mut VexinationContext) {
+    let api_end_point = "api/v1/vex?id=missing_qualifier";
+    get_response(context, &api_end_point, StatusCode::BAD_REQUEST).await;
 }
 
 #[test_context(VexinationContext)]
 #[tokio::test]
 #[ntest::timeout(60_000)]
-async fn get_vex_invalid_advisory(vexination: &mut VexinationContext) {
-    let api_endpoint = vexination.urlify("api/v1/vex?advisory=invalid_vex");
-    get_response(&api_endpoint, StatusCode::NOT_FOUND, &vexination.provider).await;
+async fn get_vex_invalid_advisory(context: &mut VexinationContext) {
+    let api_end_point = "api/v1/vex?advisory=invalid_vex";
+    get_response(context, &api_end_point, StatusCode::NOT_FOUND).await;
 }
