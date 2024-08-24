60866f3b1b89285673dfac0465c635501f0ab175
Massimiliano Dessì - (Fast Chauffeur) <desmax74@users.noreply.github.com>
2024-08-24 14:24:55 +0200

    1.1.1 (#1708)
    Signed-off-by: desmax74 <mdessi@redhat.com>

--- bombastic/model/src/data.rs
+++ bombastic/model/src/data.rs
@@ -1,6 +1,10 @@
 use std::fmt::Formatter;
+
+use cyclonedx_bom::errors::JsonReadError;
+use cyclonedx_bom::prelude::{Validate, ValidationResult};
 use tracing::{info_span, instrument};
 
+#[derive(Debug)]
 pub enum SBOM {
     #[cfg(feature = "cyclonedx-bom")]
     CycloneDX(cyclonedx_bom::prelude::Bom),
@@ -54,7 +58,7 @@ impl SBOM {
             match result {
                 Ok(spdx) => return Ok(SBOM::SPDX(spdx)),
                 Err(e) => {
-                    log::info!("Error parsing SPDX: {:?}", e);
+                    log::error!("Error parsing SPDX: {:?}", e);
                     err.spdx = Some(e);
                 }
             }
@@ -64,9 +68,43 @@ impl SBOM {
         {
             let result = info_span!("parse cyclonedx").in_scope(|| cyclonedx_bom::prelude::Bom::parse_from_json(data));
             match result {
-                Ok(bom) => return Ok(SBOM::CycloneDX(bom)),
+                // check the serial number has a value
+                Ok(bom) => match bom.serial_number {
+                    // then validate the SBOM itself
+                    // having checked the serial number is available before validating is mandatory
+                    // because it's an optional field in specs and the validation will succeed if
+                    // the serial number is missing and this isn't what we want because
+                    // serial number is mandatory for trustification to correlate properly
+                    Some(_) => match bom.validate() {
+                        Ok(validation_result) => match validation_result {
+                            ValidationResult::Passed => return Ok(SBOM::CycloneDX(bom)),
+                            ValidationResult::Failed { reasons } => {
+                                let all_reasons = reasons
+                                    .into_iter()
+                                    .map(|reason| reason.message)
+                                    .collect::<Vec<String>>()
+                                    .join(", ");
+                                log::error!("Error validating CycloneDX: {}", all_reasons);
+                                let validation_failed: serde_json::Error = serde::de::Error::custom(all_reasons);
+                                err.cyclonedx = Some(JsonReadError::from(validation_failed));
+                            }
+                        },
+                        Err(e) => {
+                            log::error!("Error validating CycloneDX: {}", e);
+                            let validation_error: serde_json::Error = serde::de::Error::custom(e);
+                            err.cyclonedx = Some(JsonReadError::from(validation_error));
+                        }
+                    },
+                    None => {
+                        let serial_number_error_message = "Error validating CycloneDX: In order for a CycloneDX SBOM to be successfully ingested the 'serialNumber' field must be populated.";
+                        log::error!("{}", serial_number_error_message);
+                        let serial_number_error: serde_json::Error =
+                            serde::de::Error::custom(serial_number_error_message);
+                        err.cyclonedx = Some(JsonReadError::from(serial_number_error));
+                    }
+                },
                 Err(e) => {
-                    log::info!("Error parsing CycloneDX: {:?}", e);
+                    log::error!("Error parsing CycloneDX: {:?}", e);
                     err.cyclonedx = Some(e);
                 }
             }
@@ -82,5 +120,50 @@ impl SBOM {
             #[cfg(feature = "cyclonedx-bom")]
             Self::CycloneDX(_) => "CycloneDX/1.3".to_string(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SBOM;
+
+    #[test]
+    fn parse_cyclonedx_valid_13() {
+        let data = include_bytes!("../../testdata/my-sbom.json");
+        let result = SBOM::parse(data);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn parse_cyclonedx_valid_14() {
+        let data = include_bytes!("../../testdata/syft.cyclonedx.json");
+        let result = SBOM::parse(data);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn parse_cyclonedx_invalid_serial_number() {
+        let data = include_bytes!("../../testdata/syft.cyclonedx.wrong-serialNumber.json");
+        let result = SBOM::parse(data);
+        assert!(result.is_err());
+        let e = result.unwrap_err();
+        assert!(e.cyclonedx.is_some());
+        assert_eq!(
+            e.cyclonedx.unwrap().to_string(),
+            "Failed to deserialize JSON: UrnUuid does not match regular expression"
+        );
+    }
+
+    #[test]
+    fn parse_cyclonedx_without_serial_number() {
+        let data = include_bytes!("../../testdata/sbom-without-serialNumber.cyclonedx.json");
+        let e = SBOM::parse(data).unwrap_err();
+        assert!(e.cyclonedx.is_some());
+        assert_eq!(e.cyclonedx.unwrap().to_string(), "Failed to deserialize JSON: Error validating CycloneDX: In order for a CycloneDX SBOM to be successfully ingested the 'serialNumber' field must be populated.");
+        assert!(e.spdx.is_some());
+        assert_eq!(
+            e.spdx.unwrap().to_string(),
+            "missing field `spdxVersion` at line 454 column 1"
+        );
     }
 }
