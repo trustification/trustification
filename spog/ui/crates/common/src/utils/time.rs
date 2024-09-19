e0cb38e714cbd8afdda513db929174c30a9ba32d
Carlos Feria <carlosthe19916@gmail.com>
2024-09-19 10:06:00 +0200

    [TC-1669] - Dashboard - Status panel - front end (#1699) (#1740)

--- spog/ui/crates/common/src/utils/time.rs
+++ spog/ui/crates/common/src/utils/time.rs
@@ -20,3 +20,14 @@ pub fn chrono_date<Tz: TimeZone>(dt: chrono::DateTime<Tz>) -> Html {
     let date = dt.date_naive();
     date.format("%b %d, %Y").into()
 }
+
+/// Formate a timestamp to represent a date.
+pub fn full_utc_date(date: OffsetDateTime) -> Html {
+    let fmt = format_description!("[year]-[month]-[day] [hour]:[minute]:[second] UTC");
+    date.format(fmt)
+        .unwrap_or_else(|err| {
+            log::info!("Failed to format date: {err}");
+            date.to_string()
+        })
+        .into()
+}
