4c153da17c7b23a5c262cb067f627c0c91d86fca
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-01-13 13:57:01 +0100

    fix: update version to 1.3.0

--- spog/ui/src/about.rs
+++ spog/ui/src/about.rs
@@ -47,7 +47,7 @@ pub fn about() -> Html {
                     <dl>
                         <dt>{"Version"}</dt>
                         <dd>
-                            {"1.2.1"}
+                            {"1.3.0"}
                         </dd>
                     </dl>
                 </div>
